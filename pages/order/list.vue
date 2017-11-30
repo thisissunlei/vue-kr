4100fe447964f927f62d23a355ed708656491dd8
dongfanai <dongfanai@krspace.cn>
2017-11-30 19:52:22 +0800

    feat:高级查询

--- pages/order/list.vue
+++ pages/order/list.vue
@@ -1,10 +1,51 @@
 <style lang="less"> 
-    
+    label{
+        width:250px;
+        line-height: 14px;
+        color: #333333;
+        padding-bottom: 10px;
+        display: inline-block;
+    }
+    form{
+        width:540px;
+        margin:0 auto;
+    }
+    .u-clearfix { zoom:1; }
+    .u-clearfix:after {
+      clear: both;
+      content: '.';
+      height: 0;
+      display: block;
+      visibility: hidden;
+    }
+    .u-input{
+        width:250px;
+        float:left; 
+        margin-bottom:10px;
+        &:nth-child(2n-1){
+          margin-right:30px;  
+        }
+    }
+    .u-search{
+        height:22px;
+        margin:16px 20px;
+        span{
+            width:22px;
+            height:22px;
+            background:url(images/upperSearch.png) no-repeat center;
+            background-size: contain;  
+            float:right;
+        }
+        
+
+    }
    
 </style>
 <template>
 <div>
-    <h1>Hello worl222!</h1>
+    <div class="u-search" @click="showSearch">
+        <span></span>   
+    </div>
     <Table  :columns="columns1" :data="data1"></Table>
     <div style="margin: 10px;overflow: hidden">
         <Button type="primary">导出</Button>
@@ -12,7 +53,72 @@
             <Page :total="pageSize" show-total show-elevator></Page>
         </div>
     </div>
-
+     <Modal
+        v-model="openSearch"
+        title="高级搜索"
+        ok-text="确定"
+        cancel-text="取消"
+        width="660"
+     >
+        <form class="u-clearfix">
+            <div class="u-input">
+               <label>订单编号</label>
+               <Input 
+                    v-model="value" 
+                    placeholder="请输入订单编号" 
+                    style="width: 250px"
+               ></Input> 
+            </div>
+            <div class="u-input">
+               <label>客户名称</label>
+               <Input 
+                    v-model="value" 
+                    placeholder="请输入客户名称" 
+                    style="width: 250px"
+               ></Input> 
+            </div>
+            <div class="u-input">
+               <label>社区名称</label>
+               <Input 
+                    v-model="value" 
+                    placeholder="请输入社区名称" 
+                    style="width: 250px"
+               ></Input> 
+            </div>
+            <div class="u-input">
+               <label>订单总额</label>
+               <Input 
+                    v-model="value" 
+                    placeholder="请输入订单总额" 
+                    style="width: 250px"
+               ></Input> 
+            </div>
+            <div class="u-input">
+               <label>订单生成时间</label>
+               <Input 
+                    v-model="value" 
+                    placeholder="请输入订单生成时间" 
+                    style="width: 250px"
+               ></Input> 
+            </div>
+            <div class="u-input">
+               <label>订单状态</label>
+               <Input 
+                    v-model="value" 
+                    placeholder="请输入订单状态" 
+                    style="width: 250px"
+               ></Input> 
+            </div>
+            <div class="u-input">
+               <label>支付状态</label>
+               <Input 
+                    v-model="value" 
+                    placeholder="请输入支付状态" 
+                    style="width: 250px"
+               ></Input> 
+            </div>
+        </form>
+    </Modal>
 </div>
 </template>
 
@@ -23,6 +129,7 @@
         data () {
             return {
                 pageSize:1,
+                openSearch:false,
                 columns1: [
                     {
                         title: '订单编号',
@@ -75,7 +182,7 @@
                                     },
                                     on: {
                                         click: () => {
-                                            this.show(params
+                                            this.openView(params
                                             )
                                         }
                                     }
@@ -97,19 +204,8 @@
                             ]);  
                         }
                     }
-                ],
-                data1: [
-                    {
-                        number: 18,
-                        name: 'John Brown',
-                        communityName:'创业大街',
-                        amount:5535,
-                        date: '2016-10-03',
-                        orderstatus:'已完成',
-                        pay:'待付款',
-                    }
-                    
                 ]
+                
             }
         },
         created:function(){
@@ -142,15 +238,17 @@
                 ]
         },
         methods:{
-            show (params) {
-                console.log('params====',params)
-                this.$Modal.info({
-                    title: 'User Info',
-                    content: '啦啦记得和'
-                })
+            showSearch (params) {
+                this.openSearch=true;
+            },
+            openView(){
+
             },
             remove (params) {
                 console.log('params222====',params)
+            },
+            changeSearch(status){
+                this.openSearch=status;
             }
            
         }

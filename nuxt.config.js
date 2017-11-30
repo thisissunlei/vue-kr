d0c59eb5c622010a6318b06e40ee8853fbd79194
张屈 <joneyphair@qq.com>
2017-11-30 13:46:28 +0800

    chore

---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  plugins:[ { src: '~plugins/iview', ssr: false }],
+  build: {
+  	//vendor:['iview'],
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+
+<div>
+
+
+<h1>Hello worl!</h1>
+
+
+
+<Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="ghost">Ghost</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="text">Text</Button>
+    <br><br>
+    <Button type="info">Info</Button>
+    <Button type="success">Success</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="error">Error</Button>
+
+
+
+  <Radio v-model="single">Radio</Radio>
+
+
+</div>
+  
+</template>

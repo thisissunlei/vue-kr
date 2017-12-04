2b1ee91002d121fe3c53262d142ccd5407854e14
马超越 <machaoyue@36kr.com>
2017-12-04 14:35:42 +0800

    add,

--- assets/apis/order.js
+++ assets/apis/order.js
@@ -4,5 +4,10 @@ module.exports = {
   'getplanmap': {
       url: 'krspace-finance-web/cmt/floor-graph/select-info?',
       method: 'get'
-  }
+  },
+  //财务--审核列表--高级查询--获取社区
+    'get-mainbill-community': {
+        url: '/krspace-finance-web/finaVerify/data/get-mainbill-community?',
+        method: 'get'
+    },
 }

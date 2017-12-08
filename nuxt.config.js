1125b70fc6e914606a6666e6d9b0a65e83d4a1ef
刘毅豪 <liuyihao@krspace.cn>
2017-12-08 17:45:17 +0800

    feature/导航ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,11 @@
 
 module.exports = {
 	plugins: [{
-		src: '~plugins/iview',
-		ssr: false
-	}],
+			src: '~plugins/iview',
+			ssr: false
+		},
+	],
+	
 	css: ['iview/dist/styles/iview.css'],
 	build: {
 		vendor: ['axios'],

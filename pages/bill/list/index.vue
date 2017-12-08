889f45068352799a27850bd6f068e2aa88ccff21
dongfanai <dongfanai@krspace.cn>
2017-12-08 15:37:15 +0800

    feat:账单列表

--- pages/bill/list/index.vue
+++ pages/bill/list/index.vue
@@ -13,6 +13,9 @@
             
             }
     }
+    .ivu-checkbox-wrapper{
+        margin-right:0;
+    }
     .ivu-table-cell{
         padding:0;
     }
@@ -48,7 +51,7 @@
         <div style="margin: 10px;overflow: hidden">
             <Button type="primary" @click="onExport">导出</Button>
             <div style="float: right;">
-                <Page :total="pageSize" show-total show-elevator></Page>
+                <Page :total="totalCount" show-total show-elevator></Page>
             </div>
         </div>
     </div>
@@ -107,32 +110,32 @@ import sectionTitle from '~/components/sectionTitle';
         },
         data () {
             return {
-                pageSize:1,
+                totalCount:0,
                 openSearch:false,
                 openSettle:false,
                 openAntiSettle:false,
-                billList:this.getTableData(),
-                params:{
+                billList:[],
+                tabParams:{
                     page:1,
                     pageSize:15
                 },
                 columns1: [
                     {
                         type: 'selection',
-                        width: 50,
+                        width: 35,
                         align: 'center'
                     },
                     {
                         title: '账单编号',
                         key: 'billNo',
                         align:'center',
-                        width:160
+                        width:115
                     },
                     {
                         title: '客户名称',
                         key: 'customerName',
                         align:'center',
-                        width:100
+                        width:190
                     },
                     {
                         title: '社区名称',
@@ -142,15 +145,15 @@ import sectionTitle from '~/components/sectionTitle';
                     },
                     {
                         title: '账单类型',
-                        key: 'billType',
+                        key: 'bizType',
                         align:'center',
-                        width:110,
+                        width:90,
                         render(h, obj){
-                            if(obj.row.billType==='MEETING'){
+                            if(obj.row.bizType==='MEETING'){
                                 return '会议室账单';
-                            }else if(obj.row.billType==='PRINT'){
+                            }else if(obj.row.bizType==='PRINT'){
                                 return '打印服务账单 ';
-                            }else if(obj.row.billType==='CONTRACT'){
+                            }else if(obj.row.bizType==='CONTRACT'){
                                 return '工位服务订单';
                             }
                         }
@@ -162,14 +165,14 @@ import sectionTitle from '~/components/sectionTitle';
                     },
                     {
                         title: '结账金额',
-                        key: 'payAmount',
+                        key: 'paidAmount',
                         align:'center'
                     },
                     {
                         title: '付款截止日期',
                         key: 'billEndTime',
                         align:'center',
-                        width:110,
+                        width:90,
                         render(h, obj){
                             let time=dateUtils.dateToStr("YYYY-MM-DD", new Date(obj.row.billEndTime));
                             return time;
@@ -177,14 +180,14 @@ import sectionTitle from '~/components/sectionTitle';
                     },
                     {
                         title: '账单状态',
-                        key: 'billStatus',
+                        key: 'billPayStatus',
                         align:'center',
                         render(h, obj){
-                                if(obj.row.payStatus==='WAIT'){
+                                if(obj.row.billPayStatus==='WAIT'){
                                     return <span class="u-txt-red">待付款</span>;
-                                }else if(obj.row.payStatus==='PAID'){
+                                }else if(obj.row.billPayStatus==='PAID'){
                                     return <span class="u-txt">已付款</span>;
-                                }else if(obj.row.payStatus==='PAYMENT'){
+                                }else if(obj.row.billPayStatus==='PAYMENT'){
                                     return <span class="u-txt-orange">未付清</span>;
                                 }
                             }
@@ -193,7 +196,7 @@ import sectionTitle from '~/components/sectionTitle';
                         title: '操作',
                         key: 'operation',
                         align:'center',
-                        width:170,
+                        width:135,
                         render:(h,params)=>{
                            return h('div', [
                                 h('Button', {
@@ -246,55 +249,15 @@ import sectionTitle from '~/components/sectionTitle';
             }
         },
         created:function(){
-        //     this.billList=[
-		// 	{
-        //         orderId:'44',
-		// 		billNo:'HYSZD201712010001',
-		// 		billType:'MEETING',
-		// 		billStartTime:1511404234000,
-		// 		billEndTime:1511063377000,
-        //         payStatus:'WAIT',
-        //         customerName:'绝地反',
-        //         communityName:'创业大街社区',
-        //         amount:'￥200',
-        //         payAmount:'￥100'
-		// 	},
-		// 	{
-        //         orderId:'45',
-		// 		billNo:'HYSZD201712010001',
-		// 		billType:'PRINT',
-		// 		billStartTime:1509372919000,
-		// 		billEndTime:1509372919000,
-		// 		payStatus:'PAID',
-        //         customerName:'绝地反击和',
-        //         communityName:'创业大街',
-        //         amount:'￥200',
-        //         payAmount:'￥100'
-		// 	},
-		// 	{
-        //         orderId:'46',
-		// 		billNo:'HYSZD201712010001',
-		// 		billType:'CONTRACT',
-		// 		billStartTime:1505704034000,
-		// 		billEndTime:1505704034000,
-		// 		payStatus:'PAYMENT',
-        //         customerName:'绝地反击和',
-        //         communityName:'创业大街',
-        //         amount:'￥200',
-        //         payAmount:'￥100'
-		// 	}
-		// ]
-           
-           
-            
+            this.getTableData();
         },
         methods:{
             showSearch (params) {
                 this.openSearch=true;
             },
             openView(params){
-                 //location.href=`./detail/${params.orderId}`;
-                 location.href='./detail/12';
+                console.log('params====>>>>',params)
+                 //location.href=`./detail/${params.billId}`;
             },
             showSettle (params) {
                 this.openSettle=true;
@@ -307,20 +270,17 @@ import sectionTitle from '~/components/sectionTitle';
                  console.log('导出')
             },
             onSelectList(data){
-                console.log('date====>>>>>0000',data)
+                //console.log('date====>>>>>0000',data)
             },
             getTableData(){
                 let data = [];
                 let params={
                     page:1,
                     pageSize:15
-                }
-                //this.params;
-                var _this=this;
+                };
                 axios.get('get-bill-list', params, r => {
-                    console.log('r', r);
-                    data=r.data;
-                    _this.totalCount=r.data.totalCount;
+                    this.billList=r.data.items;
+                    this.totalCount=r.data.totalCount;
                 }, e => {
                     console.log('error',e)
                 })

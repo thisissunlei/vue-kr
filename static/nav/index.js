454e5269322db5e2ded55c709fd93fc0e55510d5
张屈 <joneyphair@qq.com>
2017-12-12 21:27:11 +0800

    fix

--- static/nav/index.js
+++ static/nav/index.js
@@ -455,7 +455,7 @@
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) { // readyState == 4说明请求已完成
                 if (xhr.response.code<0) {
-                    window.location = '/new/login.html';
+                    window.location = 'http://optest.krspace.cn/new/login.html';
                     return;
                 }
                 callback(xhr.response)

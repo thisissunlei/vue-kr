82273f862e3d4007c0972ee00c775edf7b61be5e
张屈 <joneyphair@qq.com>
2017-12-12 22:46:37 +0800

    fix

--- plugins/iview.js
+++ plugins/iview.js
@@ -2,10 +2,9 @@ import Vue from 'vue';
 
 import iView from 'iview';
 
-
+import 'iview/dist/styles/iview.css'
 // import axios from './api.js';
 // Vue.prototype.$api = axios
 Vue.use(iView);
 
 
-import 'iview/dist/styles/iview.css'

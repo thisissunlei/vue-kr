8c508f708662aa7520d89afc7688b03691ae0851
张屈 <joneyphair@qq.com>
2017-12-13 16:28:06 +0800

    feat:logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'http://www.krspace.cn/vendors/images/logofav.png' }
     ]
   },
 

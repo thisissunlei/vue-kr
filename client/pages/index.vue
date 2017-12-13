fea02b6950d79a9e1e591ebf9cb6b138e3e43f7c
张屈 <joneyphair@qq.com>
2017-12-13 15:17:08 +0800

    feat:index

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -12,20 +12,8 @@
 <div>
 
 
-<h1>Hello worl!</h1>
+<h1 style="text-align:center;padding-top:100px;">Hello world!</h1>
 
- <Button>Default</Button>
-    <Button type="primary">Primary</Button>
-    <Button type="ghost">Ghost</Button>
-    <Button type="dashed">Dashed</Button>
-    <Button type="text">Text</Button>
-    <br><br>
-    <Button type="info">Info</Button>
-    <Button type="success">Success</Button>
-    <Button type="warning">Warning</Button>
-    <Button type="error">Error</Button>
-
- <Table :columns="columns1" :data="data1"></Table>
 
  
 </div>
@@ -35,62 +23,10 @@
 
 <script>
 
-
-    var detail=[
-
-                    {
-                        name: 'John Brown',
-                        age: 18,
-                        address: 'New York No. 1 Lake Park',
-                        date: '2016-10-03'
-                    },
-                    {
-                        name: 'Jim Green',
-                        age: 24,
-                        address: 'London No. 1 Lake Park',
-                        date: '2016-10-01'
-                    },
-                    {
-                        name: 'Joe Black',
-                        age: 30,
-                        address: 'Sydney No. 1 Lake Park',
-                        date: '2016-10-02'
-                    },
-                    {
-                        name: 'Jon Snow',
-                        age: 26,
-                        address: 'Ottawa No. 2 Lake Park',
-                        date: '2016-10-04'
-                    },
-                    {
-                        name: 'Jon Snow',
-                        age: 26,
-                        address: 'Ottawa No. 2 Lake Park',
-                        date: '2016-10-04'
-                    }
-                ]
     export default {
         data () {
             return {
-                columns1: [
-                    {
-                        title: 'Name',
-                        key: 'name'
-                    },
-                    {
-                        title: 'Age',
-                        key: 'age'
-                    },
-                    {
-                        title: 'Address',
-                        key: 'address'
-                    },
-                    {
-                        title: 'Date',
-                        key: 'date'
-                    }
-                ],
-                data1:detail
+                
             }
         }
     }

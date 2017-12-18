7b7659ccde882e2fa307c2fab49eb982413b3706
wusuling <wusuling@krspace.cn>
2017-12-18 15:25:53 +0800

    feat:菜单合同

--- client/static/nav/index.js
+++ client/static/nav/index.js
@@ -1363,8 +1363,24 @@
                     type:'vue',
                     menuItems: [
                         {
-                            primaryText: "会议室订单管理",
+                            primaryText: "订单列表",
                             router: 'orderCenter/orderManage',
+                            type:'vue',
+                            menuCode: 'iot_door_open_log',
+                            //menuCode: 'fina_meeting_order_page'
+                        },
+                    ]
+                },
+                {
+                    primaryText: "合同管理",
+                    router: '',
+                    iconName: 'icon-money',
+                    iconColor: '#79859a',
+                    type:'vue',
+                    menuItems: [
+                        {
+                            primaryText: "合同列表",
+                            router: 'contractCenter/list',
                             type:'vue',
                             menuCode: 'iot_door_open_log',
                             //menuCode: 'fina_meeting_order_page'

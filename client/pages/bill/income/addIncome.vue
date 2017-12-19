162499b11ba48731e6bbe3b60eef77d61b0c0e0f
dongfanai <dongfanai@krspace.cn>
2017-12-19 16:15:39 +0800

    fix:时间默认为当前时间

--- client/pages/bill/income/addIncome.vue
+++ client/pages/bill/income/addIncome.vue
@@ -120,7 +120,7 @@ export default{
                 customerId:'',
                 dealDate:'',
                 incomeType:'',
-                startTime:new Date()
+                startTime:''
             },
             formValidate:'formValidate',
             incomeType:[

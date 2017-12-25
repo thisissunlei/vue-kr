02b7e5ca252d2875f9123c0311a155f6357eec38
wusuling <wusuling@krspace.cn>
2017-12-25 17:31:16 +0800

    feat:loading

--- client/components/Loading.vue
+++ client/components/Loading.vue
@@ -1,8 +1,52 @@
+<style scoped>
+  .spinner {
+  width: 60px;
+  height: 60px;
+  position: relative;
+  margin: 100px auto;
+}
+ 
+.double-bounce1, .double-bounce2 {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #499df1;
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  left: 0;
+   
+  -webkit-animation: bounce 2.0s infinite ease-in-out;
+  animation: bounce 2.0s infinite ease-in-out;
+}
+ 
+.double-bounce2 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+ 
+@-webkit-keyframes bounce {
+  0%, 100% { -webkit-transform: scale(0.0) }
+  50% { -webkit-transform: scale(1.0) }
+}
+ 
+@keyframes bounce {
+  0%, 100% {
+    transform: scale(0.0);
+    -webkit-transform: scale(0.0);
+  } 50% {
+    transform: scale(1.0);
+    -webkit-transform: scale(1.0);
+  }
+}
+</style>
 <template lang="html">
   <div v-if="loading">
-    <Spin fix>
-        <Icon type="load-c" size=40 class="demo-spin-icon-load"></Icon>
-        <div>加载中...</div>
+    <Spin fix style='background:#fff;'>
+        <div class="spinner">
+          <div class="double-bounce1"></div>
+          <div class="double-bounce2"></div>
+        </div>
     </Spin>
   </div>
 </template>
@@ -12,14 +56,3 @@ export default {
   props:['loading']
 }
 </script>
-
-<style scoped>
-  .demo-spin-icon-load{
-      animation: ani-demo-spin 1s linear infinite;
-   }
-  @keyframes ani-demo-spin {
-      from { transform: rotate(0deg);}
-      50%  { transform: rotate(180deg);}
-      to   { transform: rotate(360deg);}
-  }
-</style>

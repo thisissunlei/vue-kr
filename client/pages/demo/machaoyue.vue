1191934c1b9dbc5b1cb3e76988ef21c3a081aca0
马超越 <machaoyue@36kr.com>
2017-12-26 11:46:48 +0800

    fix

---
+++ client/pages/demo/machaoyue.vue
@@ -0,0 +1,45 @@
+<style lang="less"> 
+   
+</style>
+
+
+<template>
+
+    <div>
+        
+        
+    </div>
+  
+</template>
+<script>
+import http from '~/plugins/axiosHttp';
+    export default {
+        head () {
+            return {
+                title: "合同列表"
+            }
+           
+        },
+        data () {
+            return {
+            }
+        },
+        mounted(){
+            this.getData()
+        },
+        methods:{
+            getData(){
+                http("post-contract-take-effect", {
+                    requestId:2
+                }).then( (response) => {
+                    console.log('=======>',response)
+                }).catch( (error) => {
+                    console.log('======>',error)
+                }) 
+            }
+            
+            
+        },
+        
+    }
+</script>

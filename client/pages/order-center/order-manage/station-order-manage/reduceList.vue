fd5caa7f6a94ee5ca6e969aa3a78b2dc820fff87
wusuling <wusuling@krspace.cn>
2018-01-03 10:24:00 +0800

    feat:工位订单列表

--- client/pages/order-center/order-manage/station-order-manage/reduceList.vue
+++ client/pages/order-center/order-manage/station-order-manage/reduceList.vue
@@ -1,10 +1,8 @@
 <template>
     <div class='m-bill-list'>
-            
             <div style='width:100%;padding:0 0 0 20px;'>
                     <div style='display:inline-block;width:20%;'>
-                        <Button type="primary" @click="showJoin" style='margin-right:30px;'>入驻</Button>
-                        <Button type="primary" @click="showRenew">续租</Button>
+                        <Button type="primary" @click="showReduce">减租</Button>
                     </div>
 
                     <div style='margin-bottom:10px;display:inline-block;width:80%;text-align:right;'>
@@ -24,7 +22,8 @@
                    </div>
             </div>
 
-            <Table :columns="joinOrder" :data="joinData" border style="margin:20px;marginTop:0px;"></Table>
+
+            <Table :loading="loadingStatus" :columns="joinOrder" :data="joinData" border style="margin:20px;marginTop:0px;"></Table>
             <div style="margin: 10px 20px;overflow: hidden">
                     <Buttons label='导出'  type='primary' v-on:click='outSubmit' checkAction='order_seat_export' />
                     <div style="float: right;">
@@ -36,7 +35,7 @@
                 title="高级搜索"
                 width="660"
             >
-                <HeightSearch v-on:bindData="upperChange" mask='join'></HeightSearch>
+                <HeightSearch v-on:bindData="upperChange" mask='reduce'></HeightSearch>
                 <div slot="footer">
                     <Button type="primary" @click="upperSubmit">确定</Button>
                     <Button type="ghost" style="margin-left: 8px" @click="showSearch">取消</Button>
@@ -73,13 +72,12 @@
 
 <script>
     import HeightSearch from './heightSearch';
-    import Nullify from './nullify';
     import ApplyContract from './applyContract';
+    import Nullify from './nullify';
     import dateUtils from 'vue-dateutils';
     import utils from '~/plugins/utils';
     import Message from '~/components/Message';
     import Buttons from '~/components/Buttons';
-    
 
     export default {
         name:'join',
@@ -128,38 +126,25 @@
                         align:'center'
                     },
                     {
-                        title: '服务费总额',
+                        title: '减租开始日期',
+                        key: 'startDate',
+                        align:'center',
+                        render(h, obj){
+                            let time=dateUtils.dateToStr("YYYY-MM-DD",new Date(obj.row.startDate));
+                            return time;
+                        }
+                    },
+                    {
+                        title: '减租金额',
                         key: 'rentAmount',
                         align:'center'
-                    },
-                    {
-                        title: '履约保证金',
-                        key: 'depositAmount',
-                        align:'center'
-                    },
-                    {
-                        title: '订单类型',
-                        key: 'orderType',
-                        align:'center',
-                        render(tag,params){
-                            var orderType={
-                               'IN':'入驻服务订单',
-                               'INCREASE':'增租服务订单',
-                               'CONTINUE':'续租服务订单'
-                            }
-                            for(var item in orderType){
-                                if(item==params.row.orderType){
-                                    return <span class="u-txt">{orderType[item]}</span>;
-                                }
-                            }
-                        }
                     },
                     {
                         title: '订单状态',
                         key: 'orderStatus',
                         align:'center',
                         render(tag, params){
-                            var orderStatus={
+                             var orderStatus={
                                'NOT_EFFECTIVE':'未生效',
                                'EFFECTIVE':'已生效',
                                'INVALID':'已作废'
@@ -207,7 +192,7 @@
                                     }
                                 })];
                            if(params.row.orderStatus=='NOT_EFFECTIVE'){
-                               btnRender.push(
+                               btnRender.push( 
                                 h('Button', {
                                     props: {
                                         type: 'text',
@@ -257,10 +242,10 @@
                 ]
             }
         },
-        created(){
-          var params=Object.assign({},{page:1,pageSize:15},this.$route.query);
-          this.getListData(params);
-          this.params=params;
+        created:function(){
+            var params=Object.assign({},{page:1,pageSize:15},this.$route.query);
+            this.getListData(params);
+            this.params=params;
         },
         methods:{
             showKey: function (ev) {
@@ -270,53 +255,30 @@
                 this.openSearch=!this.openSearch;
                 utils.clearForm(this.upperData);
             },
-            showJoin(){
-                window.open('/order-center/order-manage/create/join','join');
+            showNullify(params){
+                this.id=params.row.id;
+                this.openNullify=true;
             },
-            showRenew(){
-                window.open('/order-center/order-manage/create/renew','renew');
+            showReduce(){
+                window.open('/order-center/order-manage/station-order-manage/create/reduce','reduce')
+            },
+            showEdit(params){
+                window.open(`/order-center/order-manage/station-order-manage/${params.row.id}/reduce`,params.row.id)
             },
             showApply(params){
                 this.id=params.row.id;
                 this.openApply=true;
             },
             showView(params){
-                var viewName='';
-                if(params.row.orderType=='CONTINUE'){
-                    viewName='renewView';  
-                }else{
-                    viewName='joinView';   
-                }
-                window.open(`/order-center/order-manage/${params.row.id}/${viewName}`,params.row.id);
-            },
-            showNullify(params){
-                this.id=params.row.id;
-                this.openNullify=true;
-            },
-            showEdit(params){
-                let type = '';
-                switch (params.row.orderType){
-                    case 'IN':
-                        type = 'join';
-                        break;
-                    case 'INCREASE':
-                        type = 'join';
-                        break;
-                    case 'CONTINUE':
-                        type = 'renew';
-                        break;
-                    default:
-                        type = 'join';
-                        break;
-                }
-                window.open(`/order-center/order-manage/${params.row.id}/${type}`,params.row.id)
+                window.open(`/order-center/order-manage/station-order-manage/${params.row.id}/reduceView`,params.row.id);
             },
             nullifySubmit (){
+                var _this=this;
                 let params={
                     id:this.id
                 };
                  
-                 this.$http.post('join-nullify', params,r => {
+                 this.$http.post('join-nullify', params, r => {
                     this.openMessage=true;
                     this.MessageType=r.message=='ok'?"success":"error";
                     this.warn='作废成功';
@@ -325,12 +287,13 @@
                     this.openMessage=true;
                     this.MessageType="error";
                     this.warn=e.message;
-                })
+                }) 
             },
             applySubmit(){
                 let params={
                     id:this.id
                 };
+                
                  this.$http.post('apply-contract', params, r => {
                     this.openMessage=true;
                     this.MessageType=r.message=='ok'?"success":"error";
@@ -340,15 +303,15 @@
                     this.openMessage=true;
                     this.MessageType="error";
                     this.warn=e.message;
-                })  
+                })   
             },
             outSubmit (){
                 this.props=Object.assign({},this.props,this.params);
-                utils.commonExport(this.props,'/api/krspace-op-web/order-seat-add/export');
+                utils.commonExport(this.props,'/api/krspace-op-web/order-seat-reduce/export');
             },
             getListData(params){
                 var _this=this;
-                 this.$http.get('join-bill-list', params, r => {
+                 this.$http.get('reduce-bill-list', params, r => {
                     _this.totalCount=r.data.totalCount;
                     _this.joinData=r.data.items;
                     _this.openSearch=false;
@@ -365,7 +328,7 @@
                 this.getListData(params);
             },
             lowerSubmit(){
-                utils.addParams(this.params);
+                 utils.addParams(this.params);
             },
             upperChange(params,error){
                 this.upperError=error;
@@ -384,7 +347,7 @@
             },
             onChangeOpen(data){
                 this.openMessage=data;
-            }
+            },
         }
     }
 </script>

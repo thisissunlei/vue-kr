7dbc1a08f9735f57360bbbbe1ec2f21a425d089a
wusuling <wusuling@krspace.cn>
2018-01-03 15:28:34 +0800

    feat:编辑新建

--- client/pages/order-center/order-manage/general-order-manage/_watchView/joinView.vue
+++ client/pages/order-center/order-manage/general-order-manage/_watchView/joinView.vue
@@ -26,58 +26,41 @@
 <div class="g-order-detail">
 	<div class="m-detail-header">
 		<span class="u-border-left"></span>
-		工位订单详情
+		订单详情
 	</div>
 	<div class="m-detail-content">
 		<DetailStyle info="基本信息">
-			<LabelText label="客户名称：">
+			<LabelText label="订单编号：">
 			    {{basicInfo.customerName}}
 			</LabelText>
-			<LabelText label="社区名称：">
+			<LabelText label="订单状态：">
 				{{basicInfo.communityName}}
 			</LabelText>
-			<LabelText label="销售人员：">
+			<LabelText label="订单类型：">
 				{{basicInfo.salerName}}
 			</LabelText>
-			<LabelText label="创建时间：">
+			<LabelText label="订单金额：">
 				{{ctime}}
 			</LabelText>
-         </DetailStyle>
-         <DetailStyle info="租赁信息">
-			<LabelText label="租赁开始日期：">
+			<LabelText label="客户名称：">
 				{{startDate}}
 			</LabelText>
-			<LabelText label="租赁结束日期：">
+			<LabelText label="社区名称：">
 				{{endDate}}
 			</LabelText>
-			<LabelText label="分期方式：">
+			<LabelText label="销售人员：">
 				{{basicInfo.installmentTypeName}}
 			</LabelText>
-			<LabelText label="首付款日期：">
+			<LabelText label="销售日期：">
+				{{payDate}}
+			</LabelText>
+			<LabelText label=" 备注信息：">
 				{{payDate}}
 			</LabelText>
          </DetailStyle>
-         <DetailStyle info="金额信息">
-			<Table :columns="service" :data="serviceData"></Table>
-            <LabelText label="服务费总计：" style='font-weight:bold;'>
-				{{basicInfo.seatRentAmount}}  {{capitalService}}
-			</LabelText>
-            <Table :columns="treatment" :data="treatmentData"></Table>
-            <LabelText label="优惠总计：" style='font-weight:bold;'>
-				{{basicInfo.tactiscAmount}}  {{capitalTreatment}}
-			</LabelText>
-            <div>
-                <LabelText label="服务费总额：" style='color:red;'>
-                    {{basicInfo.rentAmount}}
-                </LabelText>
-                <LabelText label="履约保证金总额：" style='color:red;'>
-                    {{basicInfo.depositAmount}}
-                </LabelText>
-            </div>
-		</DetailStyle>
-		<DetailStyle info="相关合同">
+		 <DetailStyle info="相关账单">
 			<Table :columns="contract" :data="contractData"></Table>
-		</DetailStyle>
+		 </DetailStyle>
 	</div>
 </div>	
 </template>
@@ -95,102 +78,48 @@ export default {
 	},
 	head() {
         return {
-            title: '入驻详情'
+            title: '订单详情'
         }
     },
 	data(){
 		return{
 			basicInfo:{},
-			capitalService:'',
-			capitalTreatment:'',
 			ctime:'',
-			startDate:'',
-			endDate:'',
-			payDate:'',
-			service:[
-				{
-				 title: '工位/房间编号',
-                 key: 'seatName',
-                 align:'center'	
-				},
-				{
-				 title: '标准单价(元/月)',
-                 key: 'originalPrice',
-                 align:'center'	
-                },
-                {
-				 title: '开始日期',
-                 key: 'startDate',
-				 align:'center',
-				 render(h, obj){
-					 let time=dateUtils.dateToStr("YYYY-MM-DD",new Date(obj.row.startDate));
-					 return time;
-				 }	
-                },
-                {
-				 title: '结束日期',
-                 key: 'endDate',
-				 align:'center',
-				 render(h, obj){
-					 let time=dateUtils.dateToStr("YYYY-MM-DD",new Date(obj.row.endDate));
-					 return time;
-				 }	
-                },
-                {
-				 title: '小计',
-                 key: 'originalAmount',
-                 align:'center'	
-				}
-			],
-			treatment:[
-				{
-				 title: '优惠类型',
-                 key: 'tacticsName',
-                 align:'center'	
-				},
-				{
-				 title: '开始日期',
-                 key: 'freeStart',
-				 align:'center',
-				 render(h, obj){
-					 let time=dateUtils.dateToStr("YYYY-MM-DD",new Date(obj.row.freeStart));
-					 return time;
-				 }		
-				},
-				{
-				 title: '结束日期',
-                 key: 'freeEnd',
-				 align:'center',
-				 render(h, obj){
-					 let time=dateUtils.dateToStr("YYYY-MM-DD",new Date(obj.row.freeEnd));
-					 return time;
-				 }		
-				},
-				{
-				 title: '折扣比例',
-                 key: 'discountNum',
-                 align:'center'	
-				}
-            ],
             contract:[
                {
-				 title: '合同编号',
+				 title: '账单编号',
                  key: 'contractNum',
                  align:'center'	
 				},
 				{
-				 title: '合同金额',
+				 title: '账单类型',
                  key: 'rentAmount',
                  align:'center'	
 				},
 				{
-				 title: '状态',
+				 title: '账单生成日期',
+                 key: 'orderStatusTypeName',
+				 align:'center',
+				 render(h, obj){
+					let time=dateUtils.dateToStr("YYYY-MM-DD",new Date(obj.row.ctime));
+					return time;
+                  }	
+				},
+				{
+				 title: '付款截止日期',
+                 key: 'orderStatusTypeName',
+                 align:'center',
+				 render(h, obj){
+					let time=dateUtils.dateToStr("YYYY-MM-DD",new Date(obj.row.ctime));
+					return time;
+                  }		
+				},
+				{
+				 title: '账单状态',
                  key: 'orderStatusTypeName',
                  align:'center'	
-				}  
+				}   
 			],
-			serviceData:[],
-			treatmentData:[],
 			contractData:[]
 		}
 	},
@@ -203,19 +132,10 @@ export default {
 		var _this=this;
 	     this.$http.get('join-bill-detail', from, r => {
 				   _this.basicInfo=r.data;
-				   
-				   
+				      
 				   _this.ctime=r.data.ctime?dateUtils.dateToStr("YYYY-MM-DD HH:mm:SS",new Date(r.data.ctime)):'';
-				   _this.startDate=r.data.startDate?dateUtils.dateToStr("YYYY-MM-DD",new Date(r.data.startDate)):'';
-				   _this.endDate=r.data.endDate?dateUtils.dateToStr("YYYY-MM-DD",new Date(r.data.endDate)):'';
-				   _this.payDate=r.data.firstPayTime?dateUtils.dateToStr("YYYY-MM-DD",new Date(r.data.firstPayTime)):'';
-				   _this.capitalTreatment=r.data.tactiscAmount?utils.smalltoBIG(r.data.tactiscAmount):'';
-				   _this.capitalService=r.data.seatRentAmount?utils.smalltoBIG(r.data.seatRentAmount):'';
-				   _this.serviceData=r.data.orderSeatDetailVo||[];
-				   _this.treatmentData=r.data.contractTactics||[];
 				   _this.contractData=r.data.orderContractInfo[0].contractNum?r.data.orderContractInfo:[];
            	}, e => {
-           		console.log('e',e)
                 _this.$Notice.error({
                     title:e.message
                 });

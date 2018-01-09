e0561d319a1196a6f8ce990cb4c0e2a623e7d12a
马超越 <machaoyue@36kr.com>
2018-01-09 10:41:14 +0800

    feat:添加过滤器

--- client/filters/index.js
+++ client/filters/index.js
@@ -1,15 +1,10 @@
-import dateUtils from 'vue-dateutils';
+// import dateUtils from 'vue-dateutils';
 
-function dateFormat(time,fmt) {
-
-    return dateUtils.dateToStr(fmt, new Date(time))
-}
 
 function name(params) {
     
 }
 
 export default {
-    dateFormat,
     name
 };

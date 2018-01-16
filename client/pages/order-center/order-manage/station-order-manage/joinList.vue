7a5a30545b5ce472de241ba3189652a5e0e1af68
wusuling <wusuling@krspace.cn>
2018-01-16 16:23:50 +0800

    feat: 优化订单代码

--- client/pages/order-center/order-manage/station-order-manage/joinList.vue
+++ client/pages/order-center/order-manage/station-order-manage/joinList.vue
@@ -289,15 +289,15 @@
                  }
                  this.nullDisabled=true;
                  this.closeNullify();
-                 this.$http.post('join-nullify', params,r => {
-                    this.openMessage=true;
-                    this.MessageType="success";
-                    this.warn='作废成功';
-                    this.getListData(this.params);
-                }, e => {
-                    this.openMessage=true;
-                    this.MessageType="error";
-                    this.warn=e.message;
+                 this.$http.post('join-nullify', params).then((response) => {
+                     this.openMessage=true;
+                     this.MessageType="success";
+                     this.warn='作废成功';
+                     this.getListData(this.params);
+                }).catch( (error) => {
+                     this.openMessage=true;
+                     this.MessageType="error";
+                     this.warn=error.message;
                 })
             },
 
@@ -310,16 +310,16 @@
                 }
                  this.applyDisabled=true;
                  this.closeApply();
-                 this.$http.post('apply-contract', params, r => {
-                    this.openMessage=true;
-                    this.MessageType="success";
-                    this.warn='申请成功';
-                    this.getListData(this.params);
-                }, e => {
-                    this.openMessage=true;
-                    this.MessageType="error";
-                    this.warn=e.message;
-                })  
+                 this.$http.post('apply-contract', params).then((response)=>{
+                     this.openMessage=true;
+                     this.MessageType="success";
+                     this.warn='申请成功';
+                     this.getListData(this.params);
+                 }).catch((error)=>{
+                     this.openMessage=true;
+                     this.MessageType="error";
+                     this.warn=error.message;
+                 })
             },
 
             submitExport (){
@@ -344,16 +344,15 @@
             },
 
             getListData(params){
-                var _this=this;
-                 this.$http.get('join-bill-list', params, r => {
-                    _this.totalCount=r.data.totalCount;
-                    _this.joinData=r.data.items;
-                    _this.openSearch=false;
-                }, e => {
-                    _this.openMessage=true;
-                    _this.MessageType="error";
-                    _this.warn=e.message;
-                })   
+                 this.$http.get('join-bill-list', params).then((response)=>{
+                     this.totalCount=response.data.totalCount;
+                     this.joinData=response.data.items;
+                     this.openSearch=false;
+                 }).catch((error)=>{
+                     this.openMessage=true;
+                     this.MessageType="error";
+                     this.warn=error.message;
+                 })
             },
             
             onPageChange (index) {

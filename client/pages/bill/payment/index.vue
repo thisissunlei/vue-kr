7fbbc6b9c1de4e78ce760ef9c93b46c321347266
wusuling <wusuling@krspace.cn>
2018-01-24 13:55:02 +0800

    feat: 回款

--- client/pages/bill/payment/index.vue
+++ client/pages/bill/payment/index.vue
@@ -395,12 +395,12 @@ export default {
             },
 
             lowerSubmit(){
-                let customerName=this.params.customerName;
+              
                 this.page=1;
                 this.params={
                     page:1,
                     pageSize:15,
-                    customerName:customerName
+                    customerName:this.params.customerName || ''
                 }
                 utils.addParams(this.params);
             },

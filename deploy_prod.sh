36c181184405c16b552be982868e11ced9a80377
张屈 <joneyphair@qq.com>
2018-01-29 11:28:37 +0800

    fix

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -5,12 +5,12 @@ npm run build
 build_dir=kr-vue
 
 if [ $1 = 'prod01' ]; then
-rsync -rvltOD ./ ali-krspace-web-01:/data/work/${build_dir}/
+rsync -rvltOD ./ ali-krspace-web-01:/data/work/frontend/${build_dir}/
 elif [ $1 = 'prod02' ]; then
-rsync -rvltOD ./ ali-krspace-web-02:/data/work/${build_dir}/
+rsync -rvltOD ./ ali-krspace-web-02:/data/work/frontend/${build_dir}/
 elif [ $1 = 'all' ]; then
-rsync -rvltOD ./ ali-krspace-web-01:/data/work/${build_dir}/
-rsync -rvltOD ./ ali-krspace-web-02:/data/work/${build_dir}/
+rsync -rvltOD ./ ali-krspace-web-01:/data/work/frontend/${build_dir}/
+rsync -rvltOD ./ ali-krspace-web-02:/data/work/frontend/${build_dir}/
 fi
 
 

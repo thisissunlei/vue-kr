692595814f87918a573939937dddfcba098f15b7
dongfanai <dongfanai@krspace.cn>
2018-02-26 18:06:35 +0800

    feat: 推送和搜索加回车事件

---
+++ client/components/SearchForm.vue
@@ -0,0 +1,235 @@
+<template>
+<div class="ui-search-form">
+    <div :class="[flag ? className : '', 'search-content']" >
+        <div class="search-content">
+            <input 
+                type="text" 
+                autoComplete="off"  
+                @onchange = "onChange" 
+                class="search-val" 
+                :placeholder="placeholder" 
+                :name="inputName" 
+                />
+        </div>
+    </div>
+    <span class="icon-searching" @click="onSearch"></span>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        placeholder:String,
+        inputName:String,
+        searchFilter:Array,
+    },
+    data(){
+        return{
+           flag:false,
+        }  
+    },
+    methods:{
+        onChange(){
+
+        },
+        onSearch(){
+            if(this.flag){
+
+            }
+            this.flag=!this.flag;
+        }
+    }
+}
+</script>
+
+<style lang="less">
+
+.ui-search-form{
+	height: 30px;
+	border-left: 1px solid #eee;
+	border-right: 1px solid #eee;
+	padding:0 10px;
+    padding-left: 0;
+    float: right;
+	text-align: left;
+	font-size: 14px;
+	div{
+		display: inline-block;
+		vertical-align: top;
+	}
+
+	
+	.search-val{
+		border:none;
+		// border-left: 1px solid #ccc;
+		padding-left: 10px;
+		font-size: 14px;
+		width: 0;
+		&:focus {outline: none;}
+	}
+	.icon-searching{
+		z-index: 10;
+		font-size: 20px;
+		line-height: 30px;
+		color: #d4dce2;
+		float: right;
+		margin-left: 10px;
+		&:hover{
+			color:#499df1;
+		}
+	}
+	.click{
+		color: #499df1;
+	}
+	.search-name{
+		margin-left: 10px;
+		font-size: 14px;
+		display: inline-block;
+		text-align: left;
+		&:hover{
+			color: #2980b9;
+		}
+	}
+	.search-filter,.search-content,.filter-container{
+		width: 0;
+	}
+	.filter-container{
+		
+		border-right: 1px solid #eee;
+		display: inline-block;
+		color:#666666;
+		line-height: 30px;
+		em{
+			vertical-align: middle;
+			color: #666666;
+			margin-left: 5px;
+		}
+	}
+
+	.search-status{
+		z-index: 1;
+		height: 30px;
+		line-height: 30px;
+		visibility: hidden;
+		width: 0;
+		ul{
+			left: 0;
+			padding: 0;
+			margin: 0;
+			height: 0;
+			z-index: 99;
+			top: 0px;
+			visibility: hidden;
+			position: relative;
+			box-shadow: 0 1px 6px #7d7d7d;
+			padding:8px 0;
+			border-radius: 4px;
+			background-color: #fff;
+			&:before{
+				content:'';
+				border:6px solid transparent;
+				border-bottom-color:rgba(125,125,125,.3);
+				position: absolute;
+				height: 0;
+				width: 0;
+				top: -12px;
+				left: 50%;
+				transform:translateX(-50%);
+
+			}
+			&:after{
+				content:'';
+				border:6px solid transparent;
+				border-bottom-color:#fff;
+				position: absolute;
+				height: 0;
+				width: 0;
+				top: -11px;
+				left: 50%;
+				transform:translateX(-50%);
+
+			}
+
+			li{
+				padding-left: 10px;
+				background-color: #fff;
+				color: #666666;
+				height: 0;
+				cursor: pointer;
+				&:hover{
+					background-color: #f5f5f5;
+				}
+			}
+
+		}
+		.show-li{
+			visibility: visible;
+			height: auto;
+			li{
+				height: auto;
+			}
+		}
+	}
+
+	.show-form{
+		animation:show .5s forwards;
+		animation-iteration-count: 1;
+		visibility: visible;
+		width:150px;
+		.search-filter,.search-val,.search-content{
+			width: auto;
+		}
+		.search-val{
+			width: 100%;
+		}
+		.search-content{
+			width: 150px;
+		}
+		.filter-container{
+			width:100px;
+		}
+		ul{
+			width: 100px;
+		}
+	}
+	@keyframes show {
+      0% {
+         width:0;
+         opacity: 0;
+      }
+      50%{
+      	width:150;
+        opacity: 0;
+      }
+      100% {
+         width:auto;
+         opacity: 1;
+      }
+    }
+
+}
+.renderFilter{
+	width: 295px;
+	.filter-show-form{
+		width: 265px;
+		visibility: visible;
+		.search-filter,.search-val,.search-content{
+			width: auto;
+		}
+		.search-val{
+			width: 100%;
+		}
+		.search-content{
+			width: 150px;
+		}
+		.filter-container{
+			width:100px;
+		}
+		ul{
+			width: 100px;
+		}
+	}
+}
+</style>
+
+
---
+++ client/pages/app-manage/push-manage/index.vue
@@ -0,0 +1,106 @@
+<template>
+<div class="g-push-manage">
+    <SectionTitle title="推送管理" />   
+    <div class="u-search" >
+            <Button type="primary" @click="jumpCreate">新建推送</Button>
+            <SearchForm />
+    </div>
+    <div class="u-table">
+            <Table  border :columns="columns" :data="tableList"/>
+            <div style="margin: 10px;overflow: hidden">
+                <div style="float: right;">
+                    <Page 
+                        :current="page"
+                        :total="totalCount"
+                        :page-size="pageSize" 
+                        show-total 
+                        show-elevator
+                        @change="onPageChange"
+                    />
+                </div>
+            </div>
+    </div>
+
+
+</div>
+</template>
+
+<script>
+import SectionTitle from '~/components/SectionTitle';
+import SearchForm from '~/components/SearchForm';
+export default {
+  components:{
+        SectionTitle,
+        SearchForm
+  },
+  data(){
+      return{
+           page:1,
+           totalCount:0,
+           pageSize:15,
+           columns:[
+                {
+                    title: '推送标题',
+                    key: 'locationDesc',
+                    align:'center'
+                },
+                {
+                    title: '推送内容',
+                    key: 'locationDesc',
+                    align:'center'
+                },
+
+                {
+                    title: '推送目标用户',
+                    key: 'locationDesc',
+                    align:'center'
+                },
+                {
+                    title: '推送时间',
+                    key: 'locationDesc',
+                    align:'center'
+                },
+                {
+                    title: '创建人',
+                    key: 'locationDesc',
+                    align:'center'
+                },
+
+                {
+                    title: '操作',
+                    key: 'locationDesc',
+                    align:'center'
+                },
+           ],
+           tableList:[],
+      }
+  },
+  created(){
+
+  },
+  methods:{
+      jumpCreate(){
+
+      },
+      onPageChange(){
+
+      }
+  }
+
+}
+</script>
+
+<style lang="less" >
+.g-push-manage{
+    .u-search{
+        height:32px;
+        margin:16px 0;
+        padding:0 20px;
+    }
+    .u-table{
+        padding:0 20px;
+    } 
+}
+</style>
+
+

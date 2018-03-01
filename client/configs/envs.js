a519b1ea7b89493ebe43de7331659515ea7f838e
dongfanai <dongfanai@krspace.cn>
2018-03-01 15:04:29 +0800

    feat: env

--- client/configs/envs.js
+++ client/configs/envs.js
@@ -1,8 +1,8 @@
 const envs = {
     'development':{
-        'op':'http://optest01.krspace.cn',
+        'op':'http://optest02.krspace.cn',
         'local':'http://local.krspace.cn',
-        'test':'http://optest01.krspace.cn',
+        'test':'http://optest.krspace.cn',
         'rap':'http://rap.krspace.cn'
     },
     'test':{

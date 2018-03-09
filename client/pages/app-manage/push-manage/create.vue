6203198cf1a31469e745edef1c745a5204a94bf9
dongfanai <dongfanai@krspace.cn>
2018-03-09 10:58:28 +0800

    feat: 修改

--- client/pages/app-manage/push-manage/create.vue
+++ client/pages/app-manage/push-manage/create.vue
@@ -348,6 +348,9 @@ export default {
           if(form){
                this.countParams.birthMonth="1";
                this.formItem.birthMonth="1";
+          }else{
+              this.countParams.birthMonth="";
+              this.formItem.birthMonth=""; 
           }
          this.getTargetCount(this.countParams);
       },
@@ -355,6 +358,9 @@ export default {
           if(form){
               this.countParams.leader="1";
               this.formItem.leader="1";
+          }else{
+               this.countParams.leader="";
+              this.formItem.leader="";
           }
          this.getTargetCount(this.countParams);
           

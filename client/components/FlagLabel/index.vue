02de3c1ce81a67206cf22de02c57b756f921887d
wusuling <wusuling@krspace.cn>
2018-03-19 17:40:53 +0800

    featL tip

---
+++ client/components/FlagLabel/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class='ui-flag'>
+       <Tooltip :content="label" placement="right">
+            <div :class='flagClass' ref="proFlag" />
+       </Tooltip>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        label:{
+            default:'',
+            type:String
+        },
+        type:{
+            default:'lanqi',
+            type:String
+        }
+    },
+    data() {
+        return{
+            flagClass:'',
+        }
+    },
+    mounted(){
+        this.flagClass=this.type=='lanqi'?'blueFlag':'yellowFlag';
+    }
+}
+</script>
+
+<style lang='less' scoped>
+     .ui-flag{
+         float:right;
+         .blueFlag{
+            width:20px;
+            height:25px;
+            background:url(images/lanqi.png) no-repeat center;
+            background-size: contain;
+         } 
+        .yellowFlag{
+            width:20px;
+            height:25px;
+            background:url(images/huangqi.png) no-repeat center;
+            background-size: contain;
+         } 
+     }
+</style>
--- client/pages/demo/wusuling.vue
+++ client/pages/demo/wusuling.vue
@@ -1,195 +1,27 @@
 <template>
-    <div class='chart-wrap'>
-       <div class='chart-title'>
-          <p>任务列表</p>
-          <p class='chart-title-right' @click="addClick('')">
-            <Icon type="plus-circled" size="20" style="vertical-align:middle;color:#499df1;"/>
-            <span class='chart-list'>添加任务</span>
-          </p>
-       </div>
-
-       <div class='chart-detail'> 
-           <ul>
-              <li
-                v-for="item in treeData"
-                :key="item.tId"
-                class='detail-li'
-              >
-                 <div class='chart-parent' :data-box-id="item.tId">
-                      <div class='chart-left-name' @click="showClick(item.tId)">
-                        <span class='parent-icon' :id="'parent-icon'+item.tId"></span>
-                        <span class="chart-name">{{item.name}}</span>
-                      </div>
-                      <div @click="editClick(item.tId)" class='chart-edit'>
-                        <Icon type="edit" size="15" style='color:#979797;'/>
-                      </div>
-                 </div>
-                 
-                  <ul class='chart-children' style="display:none;" :id='"chart-children"+item.tId'>
-                      <li
-                        v-for="items in item.children"
-                        :key="items.tId"
-                        class='detail-li'
-                      >
-                          <div class='chart-parent' :data-box-id="items.tId">
-                              <div class='chart-left-name'>
-                                <Icon type="minus-round" size="4" style="color: #666666;"/>
-                                <span class="chart-name" style="color: #666666;">{{items.name}}</span>
-                              </div>
-                              <div @click="editClick(items.tId)" class='chart-edit'>
-                                <Icon type="edit" size="15" style='color:#979797;'/>
-                              </div>
-                          </div>
-                      </li>
-                      <p class='add-child-task' @click="addClick(item.tId)">
-                        <Icon type="plus-circled" size="20" style="vertical-align:middle;color:#499df1;"/>
-                        <span class='chart-list'>添加子任务</span>
-                      </p>
-                </ul>
-
-              </li>
-           </ul>
-       </div>
-
+    <div>
+       <FlagLabel label="123"/>
     </div>
 </template>
 
 <script>
-
+import FlagLabel from '~/components/FlagLabel';
 export default {
+    components:{
+        FlagLabel
+    },
     data() {
         return{
-           treeData:[
-             {
-               name:'信息收集',
-               tId:'1',
-               children:[
-                 {name:'意向书',tId:'2',children:[]},
-                 {name:'意向书',tId:'3',children:[]}
-               ]
-             },
-             {
-               name:'项目评估',
-               tId:'4',
-               children:[]
-             }
-           ],
 
         }
     },
     methods:{
-      addClick(id){
-         console.log('id',id); 
-      },
-      editClick(id){
-         console.log('dddd',id);
-      },
-      showClick(id){
-         var dom=document.getElementById('chart-children'+id);
-         var icon=document.getElementById('parent-icon'+id);
-         var classVal = icon.getAttribute("class");
-         if(dom.style.display=='none'){
-            dom.style.display='block';
-            classVal = classVal.concat(" iconDown");
-            icon.setAttribute("class",classVal );
-         }else{
-            dom.style.display='none';
-            classVal = classVal.replace("iconDown","");
-            icon.setAttribute("class",classVal );
-         }
-       }
+      
     }
 }
 </script>
 
 <style lang='less' scoped>
-   .chart-wrap{
-      width:246px;
-      display:inline-block;
-      border:solid 1px #E1E6EB;
-      padding-bottom:30px;
-     .chart-title{
-       width:100%;
-       height:101px;
-       text-align:center;
-       line-height:101px;
-       padding-left:12px;
-       padding-right:18px;
-       background: #F0F1F6;
-       border-bottom:solid 1px #E1E6EB;
-      }
-      .chart-detail{
-         width:100%;
-         background: #F0F1F6;
-         .detail-li{
-             .chart-parent{
-                height:70px;
-                line-height:70px;
-                border-bottom:solid 1px #E1E6EB;
-                padding-left:12px;
-                padding-right:18px;
-                .chart-left-name{
-                  display:inline-block;
-                  cursor: pointer;
-                  .chart-name{
-                    font-family: PingFang-SC-Medium;
-                    font-size: 14px;
-                    color: #499DF1;
-                    padding-left:4px;
-                    display:inline-block;
-                }
-              }
-              .chart-edit{
-                  cursor: pointer;
-                  float:right;
-              }
-              .parent-icon{
-                 display:inline-block;
-                 width:10px;
-                 height:8px;
-                 background:url(images/down.svg) no-repeat center;
-                 background-size: 100%;
-                 vertical-align: middle;
-                 margin-top:-3px;
-                 transition: all 0.5s ease;
-              }
-              .iconDown{
-                 transform: rotate(180deg);
-              }
-           }
-           .chart-children{
-               transition: all 0.5s ease;
-              .chart-left-name{
-                 padding-left:14px;
-              }
-           }
-        }   
-      }
-      p{
-          display:inline-block;
-          font-family: PingFang-SC-Medium;
-          font-size: 14px;
-          color: #666666;
-          line-height: 14px;
-          width:50%;
-          text-align:left;
-      }
-      .chart-title-right{
-         text-align: right;
-         cursor: pointer;
-      }
-      .add-child-task{
-        width:100%;
-        height:70px;
-        line-height:70px;
-        padding-left:26px;
-        border-bottom:solid 1px #E1E6EB;
-        cursor: pointer;
-      }
-      .chart-list{
-        padding-left:5px;
-        color:#499DF1;
-      }
-   }
-   
+     
+  
 </style>

933c11a0938ca238e287837620bce3df822f1ee5
dongfanai <dongfanai@krspace.cn>
2018-03-20 18:49:38 +0800

    feat: 反结算

--- client/pages/bill/list/antiSettlement.vue
+++ client/pages/bill/list/antiSettlement.vue
@@ -3,15 +3,10 @@
 	<div class="u-wrap">
 
 		<div class="u-text">
-			结算金额：<span class="u-txt-red">￥{{detail.paidAmount}}</span>
+			反结算金额：<span class="u-txt-red">￥{{detail.paidAmount}}</span>
 		</div>
-
-		 <Input
-		 	v-model="value"
-		 	placeholder="请输入反结算金额"
-		 	size="large"
-		 	style="width: 252px;margin-bottom:30px;"
-		 />
+        
+		 
 
 	</div>
 
@@ -24,28 +19,7 @@
 		name:'antiSettlement',
 		props:{
 			detail:Object
-		},
-		data (){
-			return{
-				value:'',
-			}
-		},
-		updated:function(){
-			
-        	this.$emit('formData', this.value);
-		},
-		watch: {
-			detail: {
-				deep: true,
-				handler(nextProps) {
-					if(this.detail.paidAmount){
-						this.value=this.detail.paidAmount;
-					}
-				}
-			}
-
-		},
-		
+		}
 	}
 </script>
 

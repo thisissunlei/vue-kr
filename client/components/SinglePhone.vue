872ca92044d461472161cf9176e81fd35b848877
马超越 <machaoyue@36kr.com>
2018-03-28 14:38:45 +0800

    fix:图片单张上传

--- client/components/SinglePhone.vue
+++ client/components/SinglePhone.vue
@@ -1,5 +1,4 @@
 <template>
-    <div>
     <div class="demo-upload-list" v-for="item in uploadList">
         <template v-if="item.status === 'finished'">
             <img :src="item.url">
@@ -15,32 +14,39 @@
     <Upload
         ref="upload"
         :show-upload-list="false"
+        :default-file-list="defaultList"
         :on-success="handleSuccess"
         :format="['jpg','jpeg','png']"
         :max-size="2048"
         :on-format-error="handleFormatError"
         :on-exceeded-size="handleMaxSize"
         :before-upload="handleBeforeUpload"
+        multiple
         type="drag"
-        action="/api/krspace-finance-web/activity/upload-pic"
+        action="//jsonplaceholder.typicode.com/posts/"
         style="display: inline-block;width:58px;">
         <div style="width: 58px;height:58px;line-height: 58px;">
             <Icon type="camera" size="20"></Icon>
-        </div>
-        <div class="demo-upload-list-cover" v-if="false">
-            <Icon type="ios-eye-outline" @click.native="handleView(item.name)"></Icon>
-            <Icon type="ios-trash-outline" @click.native="handleRemove(item)"></Icon>
         </div>
     </Upload>
     <Modal title="View Image" v-model="visible">
         <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%">
     </Modal>
-    </div>
 </template>
 <script>
     export default {
         data () {
             return {
+                defaultList: [
+                    {
+                        'name': 'a42bdcc1178e62b4694c830f028db5c0',
+                        'url': 'https://o5wwk8baw.qnssl.com/a42bdcc1178e62b4694c830f028db5c0/avatar'
+                    },
+                    {
+                        'name': 'bc7521e033abdd1e92222d733590f104',
+                        'url': 'https://o5wwk8baw.qnssl.com/bc7521e033abdd1e92222d733590f104/avatar'
+                    }
+                ],
                 imgName: '',
                 visible: false,
                 uploadList: []
@@ -56,68 +62,33 @@
                 this.$refs.upload.fileList.splice(fileList.indexOf(file), 1);
             },
             handleSuccess (res, file) {
-                // 上传成功后执行的内容
                 file.url = 'https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar';
                 file.name = '7eb99afb9d5f317c912f08b5212fd69a';
             },
             handleFormatError (file) {
-                // 格式校验失败执行内容
-                console.log('格式校验')
-                
                 this.$Notice.warning({
                     title: 'The file format is incorrect',
                     desc: 'File format of ' + file.name + ' is incorrect, please select jpg or png.'
                 });
             },
             handleMaxSize (file) {
-                // 校验大小
-                console.log('校验大小')
                 this.$Notice.warning({
                     title: 'Exceeding file size limit',
                     desc: 'File  ' + file.name + ' is too large, no more than 2M.'
                 });
             },
-            handleBeforeUpload (file) {
-                // 文件上传前执行
-                console.log('文件上传前',file)
-                var check = false;
-                var form = new FormData();
-                form.append('upfile', file);
-                var xhr = new XMLHttpRequest();
-                xhr.onreadystatechange = function() {
-                    if (xhr.readyState === 4) {
-                        if (xhr.status === 200) {
-                            check = true;
-                            var response = xhr.response.data;
-                            form.append('sourceservicetoken', response.token);
-                            form.append('docTypeCode', response.docTypeCode);
-                            form.append('operater', response.operater);
-                            // var xhrfile = new XMLHttpRequest();
-                            // xhrfile.onreadystatechange = function() {
-                            //     if (xhrfile.readyState === 4) {
-                            //         var fileResponse = xhrfile.response;
-                            //         if (xhrfile.status === 200) {
-                                        
-                            //             console.log('xhrfile.response.data',xhrfile.response.data)
-                            //         } 
-                            //     }
-                            // };
-                            // xhrfile.open('POST', '/api/krspace-finance-web/activity/upload-pic', true);
-                            // xhrfile.withCredentials = true;
-                            // xhrfile.responseType = 'json';
-                            // xhrfile.send(form);
-                        } 
-                    }
-                };
-
-                xhr.open('GET', '/api/krspace-finance-web/finacontractdetail/getSourceServiceToken', true);
-                xhr.responseType = 'json';
-                xhr.send(null);
-
+            handleBeforeUpload () {
+                const check = this.uploadList.length < 5;
+                if (!check) {
+                    this.$Notice.warning({
+                        title: 'Up to five pictures can be uploaded.'
+                    });
+                }
                 return check;
             }
         },
         mounted () {
+            this.uploadList = this.$refs.upload.fileList;
         }
     }
 </script>

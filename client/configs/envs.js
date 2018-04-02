2b22143bb2f83429696240bc7a75112f9c2b3a42
马超越 <machaoyue@36kr.com>
2018-04-02 11:12:04 +0800

    fix:修改本地联调环境

--- client/configs/envs.js
+++ client/configs/envs.js
@@ -1,8 +1,8 @@
 const envs = {
     'development':{
-        'op':'http://optest.krspace.cn',
+        'op':'http://optest01.krspace.cn',
         'local':'http://local.krspace.cn',
-        'test':'http://optest.krspace.cn',
+        'test':'http://optest01.krspace.cn',
         'rap':'http://rap.krspace.cn'
     },
     'test':{

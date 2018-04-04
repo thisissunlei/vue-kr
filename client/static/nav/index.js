6bdcebcf13ac22c8add57b723bf5238a45fd16fa
马超越 <machaoyue@36kr.com>
2018-04-04 18:06:58 +0800

    fix:

--- client/static/nav/index.js
+++ client/static/nav/index.js
@@ -492,7 +492,6 @@
         return {
             primaryText: "客户会员",
             router: 'member/setting-manager',
-            type:'vue',
             menuItems: [
                 {
                     primaryText: '客户',
@@ -929,33 +928,94 @@
             router: 'smarthardware',
             originUrl: './#/smarthardware/doormanage/equipmentmanage',
             menuItems: [
-                {
-                    primaryText: "门禁管理",
+                    {
+                        primaryText: "门禁管理",
+                        iconName: 'icon-card',
+                        iconColor: '#79859a',
+                        router :'',
+                        menuItems: [
+                            {
+                                primaryText: '设备管理',
+                                menuCode: 'sysDeviceDefinitionList',
+                                router: 'smarthardware/doorManage/equipmentmanage',
+                            },{
+                                primaryText: '开门记录',
+                                menuCode: 'iot_door_open_log',
+                                router: 'smarthardware/doorManage/openlog',
+                            },
+                            {
+                                primaryText: '故障报警',
+                                menuCode: 'iot_device_alarm',
+                                router: 'smarthardware/doorManage/warning',
+                            },
+                            {
+                                primaryText: '升级管理',
+                                menuCode: 'iot_upgrade_package',
+                                router: 'smarthardware/doorManage/upgrademanage',
+                            }
+                        ]
+                }, {
+                        primaryText: "打印管理",
+                        iconName: 'icon-card',
+                        iconColor: '#79859a',
+                        router :'',
+                        menuItems: [
+                            {
+                                primaryText: '打印机管理',
+                                menuCode: 'printer_manage',
+                                router: 'smarthardware/printmanage/equipmentmanage',
+                            },{
+                                primaryText: '社区打印配置',
+                                menuCode: 'print_community_config',
+                                router: 'smarthardware/printmanage/printerconfig',
+                            },
+                            {
+                                primaryText: '费用配置',
+                                menuCode: 'print_price_config',
+                                router: 'smarthardware/printmanage/priceconfig',
+                            },
+                            {
+                                primaryText: '打印记录',
+                                menuCode: 'print_record',
+                                router: 'smarthardware/printmanage/printlog',
+                            }
+                        ]
+                },{
+                    primaryText: "中央控制管理",
                     iconName: 'icon-card',
                     iconColor: '#79859a',
-                    router: '',
+                    router :'',
                     menuItems: [
+    
                         {
                             primaryText: '设备管理',
-                            menuCode: 'sysDeviceDefinitionList',
-                            router: 'smarthardware/doorManage/equipmentmanage',
-                        }, {
-                            primaryText: '开门记录',
-                            menuCode: 'iot_door_open_log',
-                            router: 'smarthardware/doorManage/openlog',
-                        },
-                        {
-                            primaryText: '故障报警',
-                            menuCode: 'iot_device_alarm',
-                            router: 'smarthardware/doorManage/warning',
-                        },
-                        {
-                            primaryText: '升级管理',
-                            menuCode: 'iot_upgrade_package',
-                            router: 'smarthardware/doorManage/upgrademanage',
+                            menuCode: 'gateway_list',
+                            router: 'smarthardware/centercontrolmanage/equipmentmanage',
+                        },{
+                            primaryText: '操作记录',
+                            menuCode: 'gateway_click_list',
+                            router: 'smarthardware/centercontrolmanage/operatelog',
                         }
                     ]
-                },],
+            },{
+                primaryText: "设备管理",
+                iconName: 'icon-card',
+                iconColor: '#79859a',
+                router :'',
+                menuItems: [
+    
+                    {
+                        primaryText: '设备查询',
+                        menuCode: 'public',
+                        router: 'smarthardware/equipmentmanage/equipmentsearch',
+                    },{
+                        primaryText: 'IP冲突检测',
+                        menuCode: 'public',
+                        router: 'smarthardware/equipmentmanage/checkrepeatip',
+                    }
+                ]
+        },
+            ],
         }
     }
     // 订单合同

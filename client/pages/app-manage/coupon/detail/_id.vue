5a65dbfeeb1b3d6d5d219d9c41daf558bf37b89b
dongfanai <dongfanai@krspace.cn>
2018-04-24 15:13:03 +0800

    feat: 免费额度

---
+++ client/pages/app-manage/coupon/detail/_id.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="g-member-welfare-detail">
+      	<SectionTitle title="会员福利-福利详情"></SectionTitle>
+        <div class="m-detail-content">
+            <DetailStyle info="基本信息">
+                  <LabelText label="创建人：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="创建时间：" style="width:1100px;">
+                    {{basicInfo.incomeType}}
+                  </LabelText>
+            </DetailStyle>
+             <DetailStyle info="领取信息">
+                  <LabelText label="会员点击次数：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                   <LabelText label="会员已领取数量：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+            </DetailStyle>
+             <DetailStyle info="展示信息">
+                  <LabelText label="福利类型：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="福利标题：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="福利面值：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <div class="u-img-content">
+                      <div class="u-img-title">福利封面：</div>
+                      <div><img :src="basicInfo.imgUrl" class="u-img-url">{{basicInfo.imgUrl?'':'无'}}</div>
+                  </div>
+                  <LabelText label="福利标签：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+            </DetailStyle>
+             <DetailStyle info="福利限制">
+                  <LabelText label="领取有效期：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="可抵扣账单期限：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="发放总数量：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="每个团队限领数量：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="每个会员限领数量：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+             </DetailStyle>
+            <DetailStyle info="领取记录">
+            
+            </DetailStyle>
+        </div>
+  </div>
+</template>
+<script>
+import SectionTitle from '~/components/SectionTitle';
+import DetailStyle from '~/components/DetailStyle';
+import LabelText from '~/components/LabelText';
+import dateUtils from 'vue-dateutils';
+
+export default {
+    components:{
+      DetailStyle,
+      LabelText,
+      SectionTitle
+    },
+    data(){
+      return{
+        basicInfo:{}
+      }
+    },
+    mounted:function(){
+        //this.getInfo();
+        GLOBALSIDESWITCH("false")
+	 },
+   methods:{
+		
+      getInfo(){
+        var _this=this;
+        let {params}=this.$route;
+        let from={
+          notificationId:params.notificationId
+        };
+
+        this.$http.get('get-notification-detail', from).then((res)=>{
+                  let data = res.data;
+                  data.jump=jumplist[data.jumpType];
+                  this.targetDetail=data.targetDetail || '';
+                
+                  this.basicInfo = data;
+        }).catch((error)=>{
+          this.$Notice.error({
+              title:error.message
+            });
+        });
+      },
+
+	}
+
+}
+</script>
+<style lang="less">
+.g-member-welfare-detail{
+  .m-detail-content{
+      padding:30px 24px;
+  }
+   .u-img-content{
+        width:100%;
+        display: inline-block;
+        margin-bottom:6px;
+        .u-img-title{
+            float:left;
+            color: #333;
+            margin-left:14px;
+        }
+        .u-img-url{
+            max-width: 132px;
+            max-width: 132px;
+            float: left;
+            margin-bottom:30px;
+        }
+    }
+    
+}
+
+</style>
+
+
+
--- client/pages/app-manage/coupon/index.vue
+++ client/pages/app-manage/coupon/index.vue
@@ -1,11 +1,186 @@
 <template>
-  
+ <div class="g-member-welfare">
+       <SectionTitle title="免费额度优惠券管理" />
+        <div class="u-search" >
+            <Button type="primary" @click="jumpCreate">新建</Button>
+            <!-- <div style='display:inline-block;float:right;padding-right:20px;'>
+                    <Input 
+                        v-model="Params.stewardName" 
+                        placeholder="请输入管家名称"
+                        style="width: 240px"
+                    />
+                    <div class='m-search' @click="submitLower">搜索</div>
+            </div> -->
+            
+        </div>
+          <div class="u-table">
+            <Table  border :columns="couponColumns" :data="tableList"/>
+            <div style="margin: 10px;overflow: hidden">
+                <div style="float: right;">
+                    <Page 
+                        :current="page"
+                        :total="totalCount"
+                        :page-size="pageSize" 
+                        show-total 
+                        show-elevator
+                        @on-change="changePage"
+                    />
+                </div>
+            </div>
+        </div>
+
+ </div>
 </template>
 <script>
+import SectionTitle from '~/components/SectionTitle';
 export default {
+  components:{
+      SectionTitle
+  },
+  data(){
+      return{
+           pageSize:15,
+           page:1,
+           totalCount:0,
+           tableList:[],
+           couponColumns:[
+                {
+                    title: '福利标题',
+                    key: 'mbrName',
+                    align:'center'
+                },
+                {
+                    title: '福利类型',
+                    key: 'mbrName',
+                    align:'center'
+                },
+                {
+                    title: '优惠面值',
+                    key: 'mbrName',
+                    align:'center'
+                },
+                {
+                    title: '发放数量',
+                    key: 'mbrName',
+                    align:'center'
+                },
+                {
+                    title: '领取数量',
+                    key: 'mbrName',
+                    align:'center'
+                },
+                {
+                    title: '领取有效期',
+                    key: 'mbrName',
+                    align:'center'
+                },
+                {
+                    title: '创建人',
+                    key: 'mbrName',
+                    align:'center'
+                },
+                {
+                    title: '操作',
+                    key: 'mbrName',
+                    align:'center',
+                    render:(h,params)=>{
+                           return h('div', [
+                               h('Button', {
+                                    props: {
+                                        type: 'text',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        color:'#2b85e4'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.jumpView(params.row)
+                                        }
+                                    }
+                                }, '详情'),
+                                h('Button', {
+                                    props: {
+                                        type: 'text',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        color:'#2b85e4'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.jumpEdit(params.row)
+                                        }
+                                    }
+                                }, '编辑'),
+                                h('Button', {
+                                    props: {
+                                        type: 'text',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        color:'#2b85e4'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.openDelete(params.row)
+                                        }
+                                    }
+                                }, '下线')
+                            ]);  
+                        }
+                },
+           ]
+      }
+  },
+  mounted(){
+      this.tableList=[
+          {
+              mbrName:1111,
+              id:1
+          }
+      ]
+  },
+  methods:{
+     jumpCreate(){
+          window.open(`/app-manage/coupon/create`,'_blank');
+     },
+     jumpView(params){
+          window.open(`/app-manage/coupon/detail/${params.id}`,'_blank');
+     },
+     jumpEdit(params){
+         window.open(`/app-manage/coupon/edit/${params.id}`,'_blank');
+     },
+     openDelete(params){
+
+     },
+    changePage(page){
+        this.tabParams.page=page;
+        this.page=page;
+        this.getTableData(this.tabParams);
+    }
+  }
+
+
   
 }
 </script>
 <style lang="less">
+.g-member-welfare{
+    .u-search{
+        height:32px;
+        margin:16px 0;
+        padding:0 20px;
+    }
+    .u-table{
+        padding:0 20px;
+    } 
+    .ivu-table-cell{
+        padding:0;
+    }
+}
+
 
 </style>
+
+

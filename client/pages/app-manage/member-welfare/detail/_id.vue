bd8697b32050555b7ebb9ec0608c913c07264d3b
dongfanai <dongfanai@krspace.cn>
2018-04-24 14:50:24 +0800

    feat: 详情

--- client/pages/app-manage/member-welfare/detail/_id.vue
+++ client/pages/app-manage/member-welfare/detail/_id.vue
@@ -1,14 +1,121 @@
 <template>
-  <div class="g-m">
-
+  <div class="g-member-welfare-detail">
+      	<SectionTitle title="会员福利-福利详情"></SectionTitle>
+        <div class="m-detail-content">
+            <DetailStyle info="基本信息">
+                  <LabelText label="创建人：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="创建时间：" style="width:1100px;">
+                    {{basicInfo.incomeType}}
+                  </LabelText>
+            </DetailStyle>
+             <DetailStyle info="领取情况">
+                  <LabelText label="会员点击次数：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+            </DetailStyle>
+             <DetailStyle info="展示信息">
+                  <LabelText label="福利类型：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="福利描述：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <LabelText label="福利面值：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                  <div class="u-img-content">
+                      <div class="u-img-title">福利封面：</div>
+                      <div><img :src="basicInfo.imgUrl" class="u-img-url">{{basicInfo.imgUrl?'':'无'}}</div>
+                  </div>
+                  <LabelText label="福利标签：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+            </DetailStyle>
+             <DetailStyle info="领取限制">
+                 <LabelText label="福利范围：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+                   <LabelText label="领取有效期：" style="width:1100px;">
+                    {{basicInfo.id}}
+                  </LabelText>
+             </DetailStyle>
+        </div>
   </div>
 </template>
 <script>
+import SectionTitle from '~/components/SectionTitle';
+import DetailStyle from '~/components/DetailStyle';
+import LabelText from '~/components/LabelText';
+import dateUtils from 'vue-dateutils';
+
 export default {
-  
+    components:{
+      DetailStyle,
+      LabelText,
+      SectionTitle
+    },
+    data(){
+      return{
+        basicInfo:{}
+      }
+    },
+    mounted:function(){
+        //this.getInfo();
+        GLOBALSIDESWITCH("false")
+	 },
+   methods:{
+		
+      getInfo(){
+        var _this=this;
+        let {params}=this.$route;
+        let from={
+          notificationId:params.notificationId
+        };
+
+        this.$http.get('get-notification-detail', from).then((res)=>{
+                  let data = res.data;
+                  data.jump=jumplist[data.jumpType];
+                  this.targetDetail=data.targetDetail || '';
+                 
+                  
+                
+                  this.basicInfo = data;
+        }).catch((error)=>{
+          this.$Notice.error({
+              title:error.message
+            });
+        });
+      },
+
+	}
+
 }
 </script>
 <style lang="less">
+.g-member-welfare-detail{
+  .m-detail-content{
+      padding:30px 24px;
+  }
+   .u-img-content{
+        width:100%;
+        display: inline-block;
+        .u-img-title{
+            float:left;
+            font-weight: 500;
+            color: #666666;
+            margin-left:14px;
+        }
+        .u-img-url{
+            max-width: 132px;
+            max-width: 132px;
+            float: left;
+            margin-bottom:30px;
+        }
+    }
+    
+}
 
 </style>
 

7e0eed82f3c56c828d29cc09a70c836fe7f471c2
马超越 <machaoyue@36kr.com>
2018-04-26 15:25:14 +0800

    fix:

--- client/pages/demo/machaoyue.vue
+++ client/pages/demo/machaoyue.vue
@@ -8,34 +8,7 @@
 <template>
 
     <div>
-         <Card >
-            <p slot="title">
-                section1
-            </p>
-            <Form ref="formItemOne" :model="formItem" :rules="ruleValidateOne" class="demo-m" label-position="left">
-                <Form-item label="客户名称" class="bill-search-class" prop="company">
-                    <i-input 
-                        v-model="formItem.company" 
-                        placeholder="请输入客户名称"
-                        style="width: 252px"
-                        :maxlength="max"
-                    />
-                </Form-item>
-                <Form-item label="相关社区" class="bill-search-class" prop="communityId"> 
-                    <Select 
-                        v-model="formItem.communityId" 
-                        placeholder="请输入社区名称" 
-                        style="width: 252px"
-                        filterable
-                        clearable
-                    >
-                        <Option v-for="item in communityList" :value="''+item.id" :key="item.id">{{ item.name }}</Option>
-                   </Select> 
-                </Form-item>
-            </Form>
-            <Button type="primary" @click="submitCreate('formItemOne')">确定1</Button>
-
-    
+        
     </div>
 </template>
 <script>

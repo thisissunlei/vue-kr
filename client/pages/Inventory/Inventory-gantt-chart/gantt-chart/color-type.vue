20c1c58104a6c5068128ba0dcdc971a284691d02
wusuling <wusuling@krspace.cn>
2018-05-08 10:13:04 +0800

    feat: article

--- client/pages/Inventory/Inventory-gantt-chart/gantt-chart/color-type.vue
+++ client/pages/Inventory/Inventory-gantt-chart/gantt-chart/color-type.vue
@@ -1,10 +1,10 @@
 <template>
   <div style="display:inline-block;margin-top: 6px;padding:0px 10px;">
-        <span style="vertical-align:middle;color:#999;font-size:14px;font-family: PingFangSC-Medium;">{{title}}</span>
         <span
             class="article"
-            :style="{background:color,verticalAlign:'middle'}"
+            :style="{background:color,verticalAlign:'middle',marginLeft:'auto',marginRight:'10px'}"
         ></span>
+        <span style="vertical-align:middle;color:#999;font-size:14px;font-family: PingFangSC-Medium;">{{title}}</span>
     </div>
 </template>
 

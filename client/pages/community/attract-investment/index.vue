8187ffa45dc2590e47597674487501a68eea2213
wusuling <wusuling@krspace.cn>
2018-05-16 16:41:06 +0800

    feat: 吸顶

--- client/pages/community/attract-investment/index.vue
+++ client/pages/community/attract-investment/index.vue
@@ -7,8 +7,13 @@
               @initData="initData"
             />
         </div>
-        <div style="margin:0 20px;">
-            <Table :columns="attractColumns" :data="attractData" border/>
+        <SlotHead :class="theHead?'header-here':'header-no'"/>
+        <div style="margin:0 20px;" class="attract-investment-table">
+            <Table :loading="loading" :columns="attractColumns" :data="attractData" border>
+                 <div slot="loading">
+                    <Loading/>
+                 </div>
+            </Table>
         </div>
         <div  class='list-footer'>
                 <Buttons label='导出'  type='primary' @click='submitExport' checkAction='seat_order_in_export'/>
@@ -27,17 +32,21 @@
 </template>
 
 <script>
+import Loading from '~/components/Loading';
 import SearchForm from '../publicPage';
 import Message from '~/components/Message';
 import Buttons from '~/components/Buttons';
 import utils from '~/plugins/utils';
 import publicFn from '../publicFn';
+import SlotHead from '../fixHead/attract-head';
 
 export default {
     components:{
+       Loading,
        SearchForm,
        Message,
-       Buttons
+       Buttons,
+       SlotHead
     },
     data() {
         return{
@@ -46,27 +55,31 @@ export default {
             openMessage:false,
             tabForms:{
                 page:1,
-                pageSize:15
+                pageSize:100
             },
+            theHead:false,
+            sideBar:true,
+            loading:false,
+            left:'',
+            width:'',
             totalCount:0,
             attractColumns:[
                 {
                     title: '商品名称',
                     key: 'name',
-                    width:150,
                     align:'center',
                 },
                 {
                     title: '商品类型',
                     key: 'type',
                     align:'center',
-                    width:110,
+                    width:100,
                 },
                 {
                     title: '工位数量',
                     key: 'capacity',
                     align:'center',
-                    width:90,
+                    width:60,
                 },
                 {
                     title: '商品定价',
@@ -95,7 +108,7 @@ export default {
                     key: 'orderList',
                     className:'current-range',
                     align:'center',
-                    width:70,
+                    width:60,
                     render(h,obj){
                         return publicFn.mergeCell(h,obj.row.orderList,'discount')
                     }
@@ -105,6 +118,7 @@ export default {
                     key: 'orderList',
                     className:'current-range',
                     align:'center',
+                    width:100,
                     render(h,obj){
                         return publicFn.mergeCell(h,obj.row.orderList,'recentStart',true)
                     }
@@ -114,6 +128,7 @@ export default {
                     key: 'orderList',
                     className:'current-range',
                     align:'center',
+                    width:100,
                     render(h,obj){
                         return publicFn.mergeCell(h,obj.row.orderList,'recentEnd',true)
                     }
@@ -141,6 +156,7 @@ export default {
                     title: '销售员',
                     key: 'orderList',
                     className:'current-range',
+                    width:100,
                     align:'center',
                     render(h,obj){
                         return publicFn.mergeCell(h,obj.row.orderList,'sellerName')
@@ -150,7 +166,53 @@ export default {
             attractData:[]    
         }
     },
+    mounted(){
+        var dom=document.getElementById('layout-content-main');
+        dom.addEventListener("scroll",this.onScrollListener);
+        window.addEventListener('resize',this.onResize);  
+        this.tableCommon();
+        var _this=this;
+        LISTENSIDEBAROPEN(function (params) {
+            _this.sideBar=params;
+        })
+    },
+    watch:{
+        sideBar:function(val){
+            this.tableCommon();
+            this.onScrollListener();
+        }
+    },
+    destroyed(){
+        var dom=document.getElementById('layout-content-main');
+        dom.removeEventListener("scroll",this.onScrollListener);
+        window.removeEventListener('resize',this.onResize); 
+    },
     methods:{
+      tableCommon(){
+        var dailyTableDom=document.querySelectorAll('div.attract-investment-table')[0];
+        if(dailyTableDom){
+            this.left=dailyTableDom.getBoundingClientRect().left;
+            this.width=dailyTableDom.getBoundingClientRect().width;
+        }  
+      },
+      onResize(){
+            this.tableCommon();
+            this.onScrollListener();
+      },
+      //滚动监听
+      onScrollListener(){            
+            var dom=document.getElementById('layout-content-main');
+            var headDom=document.querySelectorAll('div.slot-head-attract-investment')[0];
+            if(headDom){
+                headDom.style.left=this.left+'px';
+                headDom.style.width=this.width+'px';
+            }
+            if(dom.scrollTop>330){
+                this.theHead=true;
+            }else{
+                this.theHead=false;
+            }
+      },
       initData(formItem){
          this.tabForms=Object.assign({},formItem,this.tabForms);
          this.attractData=[
@@ -166,14 +228,67 @@ export default {
             },
             {
                 "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
+            },
+            {
+                "capacity":70101,"cityName":"测试内容nfhw","communityName":"测试内容7c4n","floor":76604,"investmentStatusName":"测试内容n21u","name":"测试内容1kud","quotedPrice":"测试内容0qi2","type":"测试内容63t9"
             }
         ]
         //this.getListData(this.tabForms);
       },
       getListData(params){
+           this.loading=true;
            this.$http.get('community-investment-list', params).then((response)=>{
                 this.totalCount=response.data.totalCount;
                 this.attractData=response.data.items;
+                this.loading=false;
             }).catch((error)=>{
                 this.openMessage=true;
                 this.MessageType="error";
@@ -212,17 +327,19 @@ export default {
             margin: 10px 20px;
             overflow: hidden;
         }
+        .ivu-table-cell{ 
+            padding:0;
+        }
         .current-range{
             .ivu-table-cell{ 
-                padding:0;
-            .ivu-tooltip{
-                .row-current-more{
-                    padding: 15px 0 10px 0;
-                 }
-                .noBorder{
-                    border-bottom:none;
-                  }
-               }
+                .ivu-tooltip{
+                    .row-current-more{
+                        padding: 15px 0 10px 0;
+                    }
+                    .noBorder{
+                        border-bottom:none;
+                    }
+                }
             }
             .ivu-table-cell > div{
                     border-bottom:solid 1px #e9eaec;
@@ -239,6 +356,13 @@ export default {
             .table-null{
                 line-height: 47px;
             }
+        }
+        .header-here{
+            opacity:1;
+        }
+        .header-no{
+            transition: opacity 0.2 ease;
+            opacity: 0;
         }
      }
 </style>

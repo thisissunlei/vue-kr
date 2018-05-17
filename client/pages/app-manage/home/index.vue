40978a8b757fbecff25c2fef84b195cc8a95c91c
dongfanai <dongfanai@krspace.cn>
2018-05-17 10:51:03 +0800

    feat: home

--- client/pages/app-manage/home/index.vue
+++ client/pages/app-manage/home/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>edjej</div>
+  <div class="g-app-manage-home">
+    <div class="u-home-main-part">
+
+    </div>
+    
+  </div>
 </template>
 <script>
 export default {
@@ -7,7 +12,17 @@ export default {
 }
 </script>
 <style lang="less">
-
+.g-app-manage-home{
+  .u-home-main-part{
+    background-color: #fff;
+    height: 60px;
+    border-radius: 4px;
+    width: 100%;
+    line-height: 60px;
+    margin-bottom: 20px;
+    box-shadow: 0px 1px 3px 0 rgba(0, 0, 0, 0.1);
+  }
+}
 </style>
 
 

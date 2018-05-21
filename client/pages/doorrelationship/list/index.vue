c47a509fa24e0a96c8f1e75efb36e5fb38ae9450
谭琳琳 <805148199@qq.com>
2018-05-21 16:23:37 +0800

    创建级联关系文件

---
+++ client/pages/doorrelationship/list/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="">
+       门禁级联关系
+    </div>
+</template>
+
+<script>
+// import PaidList from './paidList';
+// import WaitList from './waitList';
+//import utils from '~/plugins/utils';
+
+export default {
+   name:'billList',
+    head () {
+            return {
+                title: "门禁级联关系"
+            }
+    },
+   data(){
+       return {
+        
+       }
+   },
+   components:{
+    
+   },
+   mounted(){
+    
+   },
+   methods:{
+        
+        
+    }
+ }
+</script>

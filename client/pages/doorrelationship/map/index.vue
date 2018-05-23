1e2cac07bec71fdb7dc3935dbe6a27f7573528f8
谭琳琳 <805148199@qq.com>
2018-05-23 13:15:46 +0800

    编辑

--- client/pages/doorrelationship/map/index.vue
+++ client/pages/doorrelationship/map/index.vue
@@ -1,67 +1,170 @@
 <template>
     <div class="door-relationship">
-       <div id="myDiagramDiv" style="width:100%; height:100%; background-color: #DAE4E4;">
+       <Button  type="primary" @click="newCreateEquipmentGroup">新增设备组</Button>
+       <Modal
+            v-model="openNewCreateModal"
+            width="490"
+            title="新建设备组"
+            @on-ok="sumbmitNewCreateData">
+            <NewCreate @formData="getNewCreateData" v-if="openNewCreateModal"/>
+        </Modal>
+        <Modal
+            v-model="openEditModal"
+            width="490"
+            title="编辑设备组"
+            @on-ok="sumbmitEditData">
+            <EditForm @formData="getEditForm" v-if="openEditModal" :initialData="editInitailData"/>
+        </Modal>
+        <div id="myDiagramDiv" style="width:1000px; height:500px; background-color: #DAE4E4;">
 
-       </div>
+        </div>
     </div>
 </template>
 
 <script>
-// import PaidList from './paidList';
-// import WaitList from './waitList';
-//import utils from '~/plugins/utils';
-
-
+import Buttons from '~/components/Buttons';
+import NewCreate from './newCreate';
+import EditForm from './editForm';
 
 export default {
-   name:'billList',
+   name:'doorRelationship',
     head () {
-            return {
-                title: "门禁级联关系"
-            }
+        return {
+            title: "门禁级联关系"
+        }
     },
    data(){
        return {
-        
+            openNewCreateModal :false,
+            openEditModal : false,
+            newCreateData : {},
+            editData : {},
+            callback:null,
+            cancelCallback:null,
+            myDiagram:null,
+            model : null,
+            editInitailData : {},
        }
    },
    components:{
-    
+    NewCreate,
+    EditForm
    },
    mounted(){
+       let _this =this;
         var $ = go.GraphObject.make;
-        var myDiagram = $(go.Diagram, "myDiagramDiv",
+        this.myDiagram = $(go.Diagram, "myDiagramDiv",
         {
+            // start everything in the middle of the viewport
+            initialContentAlignment: go.Spot.Center,
             "undoManager.isEnabled": true, // enable Ctrl-Z to undo and Ctrl-Y to redo
-            layout: $(go.TreeLayout, // specify a Diagram.layout that arranges trees
-                    { angle: 90, layerSpacing: 35 })
+            // have mouse wheel events zoom in and out instead of scroll up and down
+            "toolManager.mouseWheelBehavior": go.ToolManager.WheelZoom,
         });
         // the template we defined earlier
-        myDiagram.nodeTemplate =
-        $(go.Node, "Horizontal",
-            { background: "#44CCFF" },
-            $(go.Picture,
-            { margin: 10, width: 50, height: 50, background: "red" },
-            new go.Binding("source")),
-            $(go.TextBlock, "Default Text",
+        this.myDiagram.nodeTemplate =
+        $(go.Node, "Auto",
+            // { background: "#44CCFF" },
+            new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
+            $(go.Shape, "RoundedRectangle",
+                {
+                    parameter1: 10,  // the corner has a large radius
+                    fill: $(go.Brush, "Linear", { 0: "rgb(254, 201, 0)", 1: "rgb(254, 162, 0)" }),
+                    stroke: null,
+                    portId: "",  // this Shape is the Node's port, not the whole Node
+                    fromLinkable: true, fromLinkableDuplicates: true,
+                    toLinkable: true,toLinkableDuplicates: true,
+                    cursor: "pointer"
+                }),
+            $(go.TextBlock, "groupName",
             { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
-            new go.Binding("text", "name"))
+             new go.Binding("text","groupName").makeTwoWay())
         );
 
-        var model = $(go.TreeModel);
-        model.nodeDataArray =
-        [
-        { key: "1",              name: "Don Meow",   source: 'map/images/seal.png'},
-        { key: "2", parent: "1", name: "Demeter",    source: "cat2.png" },
-        { key: "3", parent: "1", name: "Copricat",   source: "cat3.png" },
-        { key: "4", parent: "3", name: "Jellylorum", source: "cat4.png" },
-        { key: "5", parent: "3", name: "Alonzo",     source: "cat5.png" },
-        { key: "6", parent: "2", name: "Munkustrap", source: "cat6.png" }
-        ];
-        myDiagram.model = model;
+        // unlike the normal selection Adornment, this one includes a Button
+        this.myDiagram.nodeTemplate.selectionAdornmentTemplate =
+        
+            $(go.Adornment, "Spot",
+                $(go.Panel, "Auto",
+                $(go.Shape, { fill: null, stroke: "blue", strokeWidth: 1 }),
+                $(go.Placeholder)  // a Placeholder sizes itself to the selected Node
+                ),
+                // the button to create a "next" node, at the top-right corner
+                $("Button",
+                {
+                    alignment: go.Spot.TopRight,
+                    click: _this.editItemData,  // this function is defined below
+                    
+                },
+                $(go.TextBlock, "编辑",  // the Button content
+                    { font: "normal 10pt sans-serif" }),
+                $(go.Shape, "PlusLine", { width: 6, height: 6 })
+                ) // end button
+            ); // end Adornment
+
+        var dateTemplate = { "nodeKeyProperty": "id",
+                            "nodeDataArray": [
+                                { "id": 0, "loc": "120 120", "groupName": "设备组1" ,"memo":"111233"},
+                                { "id": 1, "loc": "330 120", "groupName": "First down" },
+                                { "id": 2, "loc": "226 376", "groupName": "First up" },
+                                { "id": 3, "loc": "60 276", "groupName": "Second down" },
+                                { "id": 4, "loc": "226 226", "groupName": "Wait" }
+                            ],
+                            "linkDataArray": [
+                                { "from": 0, "to": 1, "curviness": 20 },
+                                { "from": 1, "to": 2 },
+                                { "from": 1, "to": 4 },
+                                { "from": 2, "to": 0 },
+                                { "from": 2, "to": 3 },
+                                { "from": 3, "to": 0 },
+                                { "from": 4, "to": 0 },
+                            ]
+                            }
+        this.model = go.Model.fromJson(dateTemplate);
+        
+        this.myDiagram.model = this.model;
+
+        
    },
    methods:{
-        
+        newCreateEquipmentGroup(){
+           
+            this.openNewCreateModal = !this.openNewCreateModal;
+            
+        },
+        getNewCreateData(form,callback,cancel){
+            
+            this.newCreateData=form;
+
+        },
+        getEditForm(form){
+            this.editData=form;
+        },
+        sumbmitNewCreateData(){
+            console.log("newCreateData",this.newCreateData);
+            var newCreateData = Object.assign({},this.newCreateData)
+            this.myDiagram.startTransaction("make new node");
+            this.myDiagram.model.addNodeData(newCreateData);
+            this.myDiagram.commitTransaction("make new node");
+            // 获取model数据
+            console.log("this.myDiagram.model.toJson()",this.myDiagram.model.toJson())
+        },
+        sumbmitEditData(){
+            console.log("editDate",this.editData);
+            
+        },
+        editItemData (e, obj){
+            console.log("e",e,"obj",obj,obj.part.adornedPart.data);
+            this.editInitailData = obj.part.adornedPart.data;
+            this.openEditFormModal();
+
+        },
+        openEditFormModal(){
+            this.openEditModal = !this.openEditModal
+        }
+
+
+
         
     }
  }
@@ -71,6 +174,4 @@ export default {
         width:100%;
         height:100%;
     }
-
-
 </style>
---
+++ client/pages/doorrelationship/map/newCreate.vue
@@ -0,0 +1,69 @@
+
+<template>
+<div class="g-high-search">
+    <Form  
+        ref="formValidate" 
+        :model="formItem" 
+        label-position="right"  
+        :rules="ruleValidate"
+        :label-width="100">
+        <FormItem label="设备组名称："   prop="groupName">
+            <Input
+                v-model="formItem.groupName"
+                placeholder="设备组名称"
+                style="width: 300px"
+            />
+        </FormItem>
+        <FormItem label="组描述：" prop="memo">
+            <Input
+                v-model="formItem.memo"
+                placeholder="组描述"
+                style="width: 300px"
+            />
+        </FormItem>
+       
+    </Form>
+</div>
+</template>
+<script>
+export default{
+    name:'newCreate',
+    data (){
+		return{
+			formItem:{
+                groupName:'',
+                memo:'',
+            },
+            formValidate:'formValidate',
+            ruleValidate : {
+                groupName: [
+                        { required: true, message: '设备组名称必填', trigger: 'blur' },
+                        { type: 'string', max: 10, message: '组名称最长10个字符', trigger: 'blur' }
+                    ],
+                memo: [
+                        { type: 'string', max: 20, message: '描述最长20个字符', trigger: 'blur' }
+                    ],
+            }
+		}
+    },
+     props:{
+         
+    },
+    methods:{
+        
+    },
+    updated:function(){
+
+      var formValidate = this.$refs[this.formValidate];
+       let callback = formValidate.validate;
+       let cancel = formValidate.resetFields;
+       
+       this.$emit('formData', this.formItem,callback,cancel);  
+       
+    }
+
+}
+</script>
+<style lang="less">
+
+</style>

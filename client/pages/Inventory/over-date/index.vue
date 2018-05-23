5cce2195bfcb8bfb3ff2e269693b1f1ee31c10c7
马超越 <machaoyue@36kr.com>
2018-05-23 14:03:34 +0800

    fix: 逾期未付

--- client/pages/Inventory/over-date/index.vue
+++ client/pages/Inventory/over-date/index.vue
@@ -6,9 +6,22 @@
       @initData="initData"
       identify='daily'
     />
-    <div class='daily-tab'>
+    <div class='enter-filed-table' id="daily-inventory-table-list">
         <span class="line"></span>
-        <Table border :columns="columns" :data="tableList" />
+        <Table :loading="loading" border stripe :columns="columns" :data="dailyOldData">            
+            <div slot="loading">
+                    <Loading/>
+            </div> 
+        </Table>
+        <SlotHead :class="theHead?'header-here':'header-no'" indentify="daily"/>
+        <div class='spin-position-fix' v-if="spinLoading">
+            <Spin fix size="large"></Spin>
+        </div>
+        <div  :class="theEnd?'list-footer':'on-export-middle'" :style="{left:theEnd?0:left+'px',width:width+'px'}">
+            <div style="display:inline-block;">
+                <Button type='primary' @click='submitExport'>导出(共{{totalCount}}条)</Button>
+            </div>
+        </div>
     </div>
     <Message 
         :type="MessageType" 
@@ -27,16 +40,31 @@ import dateUtils from 'vue-dateutils';
 import utils from '~/plugins/utils';
 import SearchForm from './searchForm';
 import Message from '~/components/Message';
-
+import SlotHead from '../slotHead';
+import Loading from '~/components/Loading';
+var layoutScrollHeight=0;
     export default {
         name: 'EnterField',
         components:{
             SearchForm,
-            Message
+            SlotHead,
+            Message,
+            Loading
         },
         data () {
             return {  
+                dailyOldData:[],
+                totalCount:'',
+                loading:true,
+                spinLoading:false,
                 tabsValue:'dailyList', 
+                theHead:false,
+                theEnd:true,
+                sideBar:true,
+                left:'',
+                width:'',
+
+
                 tabForms:{
                     page:1,
                     pageSize:100,
@@ -106,10 +134,47 @@ import Message from '~/components/Message';
             }
         },
         mounted(){
-            this.getData(this.endParams);
-            // GLOBALSIDESWITCH('false');
+            if(this.tabForms.cityId){
+                this.getCommonParam();
+                this.getData(this.tabForms); 
+            }   
+            var dom=document.getElementById('layout-content-main');
+            var dailyTableDom=document.getElementById('daily-inventory-table-list');
+            if(dailyTableDom){
+                this.left=dailyTableDom.getBoundingClientRect().left;
+                this.width=dailyTableDom.getBoundingClientRect().width;
+            }  
+            dom.addEventListener("scroll",this.onScrollListener);
+            window.addEventListener('resize',this.onResize);  
+            var _this=this;
+            LISTENSIDEBAROPEN(function (params) {
+                _this.sideBar=params;
+            })
+
+        },
+        watch:{
+            sideBar:function(val){
+                var dailyTableDom=document.getElementById('daily-inventory-table-list');
+                this.left=dailyTableDom.getBoundingClientRect().left;
+                this.width=dailyTableDom.getBoundingClientRect().width;
+                this.onScrollListener();
+            },
+            tabForms:function(val,old){
+                this.getCommonParam();
+                this.getData(this.tabForms); 
+            }
+        },
+        destroyed(){
+            var dom=document.getElementById('layout-content-main');
+            dom.removeEventListener("scroll",this.onScrollListener);
+            window.removeEventListener('resize',this.onResize); 
         },
         methods:{
+            getCommonParam(){
+                this.tabForms.page=1;
+                this.dailyOldData=[];
+                this.loading=true;
+            },
             //搜索
             searchClick(formItem){
                 this.tabForms=Object.assign({},this.tabForms,formItem);
@@ -131,10 +196,28 @@ import Message from '~/components/Message';
                 this.dataParams(this.tabForms);
             },
             getData(params){
-                this.$http.get('getDueList', params).then((res)=>{
-                    this.tableList = []
+                // this.$http.get('getDueList', params).then((res)=>{
+                //     this.tableList = []
+                // }).catch((error)=>{
+                //     this.tableList = []
+                //     this.openMessage=true;
+                //     this.MessageType="error";
+                //     this.warn=error.message;
+                // }) 
+
+                this.$http.get('getDailyInventory', params).then((res)=>{
+                    this.tableList=res.data.items;
+                    this.dailyIndentify=res.data.items;
+                    this.totalCount=res.data.totalCount;
+                    this.loading=false;
+                    this.spinLoading=false;
+                    this.dailyOldData=this.dailyOldData.concat(this.tableList);
+
+                    this.$nextTick(() => {
+                        var div = document.getElementById('layout-content-main')
+                        div.scrollTop = layoutScrollHeight
+                    }) 
                 }).catch((error)=>{
-                    this.tableList = []
                     this.openMessage=true;
                     this.MessageType="error";
                     this.warn=error.message;
@@ -144,6 +227,43 @@ import Message from '~/components/Message';
             onMessageChange(data){
                 this.openMessage=data;
             },
+            submitExport(){
+                console.log('submitExport')
+            },
+            //滚动监听
+        onScrollListener(){            
+            var dom=document.getElementById('layout-content-main');
+            var headDom=document.getElementById('slot-head-daily-inventory');
+            if(headDom){
+                headDom.style.left=this.left+'px';
+                headDom.style.width=this.width+'px';
+            }
+            if(dom.scrollTop>330){
+                this.theHead=true;
+            }else{
+                this.theHead=false;
+            }
+            if(!this.theEnd && (dom.scrollTop + dom.clientHeight >= dom.scrollHeight)){
+                this.theEnd=true;
+            }
+            if(this.theEnd && (dom.scrollTop + dom.clientHeight < dom.scrollHeight)){
+                this.theEnd=false;
+            }
+
+            layoutScrollHeight=dom.scrollTop;
+            var totalPage=Math.ceil(this.totalCount/this.tabForms.pageSize);
+            if(dom.scrollHeight-dom.scrollTop-dom.clientHeight<10){
+                if(this.tabForms.page==totalPage){
+                    return ;
+                }
+                if(!this.dailyIndentify.length){
+                    return ;
+                }
+                this.spinLoading=true;
+                this.tabForms.page=Number(this.tabForms.page)+1;
+                this.getData(this.tabForms);
+            }
+        },
         }
     }
 </script>
@@ -233,6 +353,69 @@ import Message from '~/components/Message';
         }
     }
 }
-
-
+     .enter-filed-table{
+            
+            padding:20px;
+            padding-bottom:77px; 
+            margin-top: 30px;
+            .ivu-tooltip{
+                width:100%
+            }
+            .ivu-tooltip-rel{
+                width:100%
+            }
+            .ivu-tooltip-popper{
+                word-break: break-all;
+                word-wrap: break-word;
+                max-width:200px;
+            }
+            .ivu-tooltip-inner{
+                white-space: normal;
+            }
+            .spin-position-fix {
+                position: relative;
+                .ivu-spin-fix{
+                    height: 50px;
+                    bottom:60px;
+                    background-color:transparent;
+                }
+            }
+            .list-footer{
+                padding:20px 0 20px 0;
+                position: absolute;
+                bottom: 0px;
+            }
+            .on-export-middle{
+                position: fixed;
+                bottom: 53px;
+                z-index: 999;
+                left: 20px;
+                padding:17px 0 20px 0;
+            }
+            .priceClass{
+                .ivu-table-cell{
+                    padding:0;
+                    padding-right:5px;
+                }
+            }
+            .statusClass{
+                .ivu-table-cell{
+                    padding:0 5px;
+                }
+            }
+            .daily-here-date{
+                .ivu-table-cell{
+                    padding:0 10px;
+                }
+            }
+            .header-here{
+                opacity:1;
+            }
+            .header-no{
+                transition: opacity 0.2 ease;
+                opacity: 0;
+            }
+        }
 </style>
+
+

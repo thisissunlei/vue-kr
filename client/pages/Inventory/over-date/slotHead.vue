a31e7d16ac29264af9856fd06ab3fc3d57028d97
马超越 <machaoyue@36kr.com>
2018-05-24 15:56:47 +0800

    fix:

--- client/pages/Inventory/over-date/slotHead.vue
+++ client/pages/Inventory/over-date/slotHead.vue
@@ -36,7 +36,7 @@
      display: flex; 
      white-space: normal;
     word-break: break-all;
-     height:55px;
+     height:50px;
      /*line-height:40px;*/
      background-color: #f8f8f9;
      z-index:999;
@@ -53,13 +53,13 @@
         &:nth-child(2),&:nth-child(8){
             width:110px;
         }
-        &:nth-child(3),&:nth-child(4),&:nth-child(5),&:nth-child(6),&:nth-child(10),&:nth-child(9),&:nth-child(11){
+        &:nth-child(3),&:nth-child(4),&:nth-child(5),&:nth-child(6),&:nth-child(11){
             width:80px;
         }
         &:nth-child(7){
             width:100px;
         }
-        &:nth-child(1){
+        &:nth-child(1),&:nth-child(10),&:nth-child(9){
             width:100%;
             flex: 1.5;
             /*-moz-box-flex: 1; */

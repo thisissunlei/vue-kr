ad094e2f2b3dc1bbafb0b27e90454464b9bd2fae
wusuling <wusuling@krspace.cn>
2018-05-24 18:30:38 +0800

    feat: price

--- client/pages/Inventory/floor-plan/searchForm.vue
+++ client/pages/Inventory/floor-plan/searchForm.vue
@@ -64,11 +64,6 @@ export default {
           communityId:'',
           floor:'',
           currentDate:publicFn.getToDay()
-       },
-       isStore:{
-           city:'',
-           community:'',
-           floor:''
        }
     }
   },

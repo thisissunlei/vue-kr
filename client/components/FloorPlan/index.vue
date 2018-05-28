2ab3ce2eda925d756524de2544e83a25c24eabc8
wusuling <wusuling@krspace.cn>
2018-05-28 16:58:06 +0800

    feat: op

--- client/components/FloorPlan/index.vue
+++ client/components/FloorPlan/index.vue
@@ -54,7 +54,7 @@ var img='';
     mounted(){
         //背景图
         img=new Image();
-        img.src="http://optest03.krspace.cn"+this.data.graphFilePath;
+        img.src="http://op.krspace.cn"+this.data.graphFilePath;
         img.setAttribute("crossOrigin",'Anonymous');
         img.addEventListener('load',this.imgLoad);
     },

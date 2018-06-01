202628e69231bdfc250df16d5876d1beb5e11382
马超越 <machaoyue@36kr.com>
2018-06-01 11:37:13 +0800

    fix:

--- client/assets/apis/bill/makeInvoice.js
+++ client/assets/apis/bill/makeInvoice.js
@@ -2,27 +2,27 @@
 export default  {
     //账单中心--获取反结算信息
     'get-invoice-list': {
-      url: '/ben/csr-invoice-list',
+      url: '/api/order/csr-invoice-list',
       method: 'get'
     },
     //开票接口
     'post-make-invoice': {
-      url: 'csr-finance-openTicket',
+      url: '/api/order/csr-finance-openTicket',
       method: 'post'
     },
     //发票列表统一接口
     'invoice-list-unified': {
-      url: '/ben/csr-invoice-list',
+      url: '/api/order/csr-invoice-list',
       method: 'get'
     },
     //根据客户id获取可开票数据
     'get-make-invoice-detail': {
-      url: '/ben/csr-invoice-qualification/info',
+      url: '/api/order/csr-invoice-qualification/info',
       method: 'get'
     },
      //根据客户id获取开票信息
     'get-make-invoice-detail-table': {
-      url: '/ben/csr-invoice-amountDetail',
+      url: '/api/order/csr-invoice-amountDetail',
       method: 'get'
     }
    

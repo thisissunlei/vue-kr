cd5913382486838c268516e9baeafa55b9d26d9d
wusuling <wusuling@krspace.cn>
2018-06-04 10:08:00 +0800

    feat: code码

--- client/static/nav/index.js
+++ client/static/nav/index.js
@@ -731,7 +731,7 @@
                         primaryText: '逾期未付',
                         router: 'inventory/over-date-unpaid',
                         type:'vue',
-                        menuCode: 'rent_no_pay',
+                        menuCode: 'overdate_no_pay',
                     },
                     {
                         primaryText: '预约参观',

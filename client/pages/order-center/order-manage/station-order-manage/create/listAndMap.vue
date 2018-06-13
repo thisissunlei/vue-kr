e781f36d3445f53915c3449351a4e1d421452c56
wusuling <wusuling@krspace.cn>
2018-06-13 11:09:32 +0800

    feat: 选择

---
+++ client/pages/order-center/order-manage/station-order-manage/create/listAndMap.vue
@@ -0,0 +1,58 @@
+
+<template>
+   <div class="order-map-list">
+       <Tabs size="default" value="list" @on-click="tabsClick" :animated="false">
+            <TabPane label="以列表方式选择" name="list">
+                123
+            </TabPane>
+            <TabPane label="以平面图方式选择" name="map">
+                456
+            </TabPane>
+        </Tabs>
+   </div>
+</template>
+
+<script>
+import planMap from '~/components/PlanMap.vue';
+export default { 
+    props:{
+        params:{
+            type:Object,
+            default:{}
+        },
+        floors:{
+            type:Object,
+            default:()=>[]
+        },
+        stationData:{
+            type:Object,
+            default:{}
+        }
+    },  
+    components:{
+       planMap
+    }, 
+    data() {
+        return{
+          
+        }
+    },
+    mounted(){
+       
+    },
+    methods:{
+     
+    }
+}
+</script>
+
+<style lang="less">
+  .order-map-list{
+      .ivu-tabs-nav .ivu-tabs-tab{
+            width: 50%;
+            text-align: center;
+            line-height: 35px;
+            padding: 8px 20px;   
+        }
+  }
+</style>

f4e82db8268b02f54a23d62364a290ab07ee9ee0
wusuling <wusuling@krspace.cn>
2018-06-14 16:00:35 +0800

    feat: import

--- client/pages/demo/wusuling.vue
+++ client/pages/demo/wusuling.vue
@@ -10,11 +10,9 @@
             minCalibration="50"
         />
         <ImportFile 
-            url="//jsonplaceholder.typicode.com/posts/"
             @downFile="downFile"
             @close="close"
-            @success="success"
-            @error="error"
+            @upload="upload"
         />
     </div>
 </template>
@@ -36,7 +34,8 @@ export default {
                 actualStartTime:1520726400000,
                 planEndTime:1521504000000,
                 planStartTime:1520640000000
-            }
+            },
+            file:{}
         }
     },
     methods:{
@@ -46,10 +45,50 @@ export default {
        close(){
 
        },
-       error(){
+       submitFile(){
+         let _this = this;
+		 var form = new FormData();
+		 form.append('file',this.file);
+		 if(!this.file.name){
+             this.$Notice.error({
+                title:'请选择上传文件'
+             });
+			 return false;
+		 }
+		 var xhr = new XMLHttpRequest();
+		 xhr.onreadystatechange = function() {
+			 if (xhr.readyState === 4) {
+				 if (xhr.status === 200) {
+					 if (xhr.response && xhr.response.code > 0) {
+						  _this.success(xhr.response);
+					 } else {
+                          _this.error(xhr.response);
+                         _this.$Notice.error({
+                            title:xhr.response.message
+                         });
+					 }
+				 }else {
+                     _this.$Notice.error({
+                        title:'上传失败'
+                     });
+				 }
+			 }
+		 };
+		 xhr.open('POST','//jsonplaceholder.typicode.com/posts/', true);
+		 xhr.responseType = 'json';
+		 xhr.send(form);
+       },
+       success(res){
 
        },
-       success(){
+       error(res){
+
+       },
+       upload(file){
+           this.file=file;
+           this.judgeRepeat();
+       },
+       judgeRepeat(){
            
        }
     }

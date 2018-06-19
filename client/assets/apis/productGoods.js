805b0fbf312f740e173c25d9153e19a1818f5d92
dongfanai <dongfanai@krspace.cn>
2018-06-19 10:10:46 +0800

    feat: api

--- client/assets/apis/productGoods.js
+++ client/assets/apis/productGoods.js
@@ -11,6 +11,26 @@ export default  {
         url: '/api/krspace-finance-web/krmting/room/add',
         method: 'get'
     },
+    //会议室商品--删除
+    'delete-krmting-room': {
+        url: '/api/krspace-finance-web/krmting/room/delete',
+        method: 'post'
+    },
+    //会议室商品--楼层
+    'get-krmting-room-floor-list': {
+        url: '/api/krspace-finance-web/krmting/room/floor/list',
+        method: 'get'
+    },
+    //会议室商品--会议室设备
+    'get-krmting-room-device-list': {
+        url: '/api/krspace-finance-web/krmting/room/device/list',
+        method: 'get'
+    },
+    //会议室商品--会议室详情
+    'get-krmting-room-detail': {
+        url: '/api/krspace-finance-web/krmting/room/detail',
+        method: 'get'
+    },
     
   }
   

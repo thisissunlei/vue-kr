a33cfcaf7268abb55cb3bad16dd7a447ac0798ba
yihaoliu <liuyihao@krspace.cn>
2018-06-26 21:59:38 +0800

    feat: 菜单修改

--- client/assets/apis/managementTool.js
+++ client/assets/apis/managementTool.js
@@ -1,8 +1,7 @@
 export default {
     //企业管理员列表
     "get-validate-list": {
-        // url: "/apis/krspace-op-web/validate/list",
-        url: "/validate/list",
+        url: "/api/krspace-op-web/validate/list",
         method: "get"
     }
 };

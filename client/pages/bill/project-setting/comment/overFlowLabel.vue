799e41b6573232c13e76c77b797481cfc936ad99
yihaoliu <liuyihao@krspace.cn>
2018-06-29 20:21:01 +0800

    feat: 订单完成

---
+++ client/pages/bill/project-setting/comment/overFlowLabel.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="overflow-label" :id="overflowId">
+      {{label}}
+  </div>
+</template>
+
+<script>
+
+export default {
+ 
+  components: {
+  },
+  props:{
+      label:{
+          type:String,
+          default:''
+      }
+  },
+//   watch:{
+//       label(){
+//           console.log(this.label.length)
+//       }
+//   },
+  data () {
+    return {
+        overflowId:'overflow'+this._uid
+    }
+  },
+  mounted(){
+      this.$nextTick(()=>{
+          console.log()
+          const labelDom = document.getElementById(this.overflowId);
+          const labelHeight = labelDom.clientHeight;
+          if(labelHeight>35){
+              labelDom.className='overflow';
+          }
+          console.log(labelHeight,"oooooo")
+      })
+    //   console.log(this.label,"pppppooooo")
+      
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+
+    .overflow{position: relative; max-height: 35px;overflow: hidden;}
+    .overflow::after{content: "..."; position: absolute; bottom: 0; right: 0; padding-left: 40px;
+    background: -webkit-linear-gradient(left, transparent, #fff 55%);
+    background: -o-linear-gradient(right, transparent, #fff 55%);
+    background: -moz-linear-gradient(right, transparent, #fff 55%);
+    background: linear-gradient(to right, transparent, #fff 55%);
+    }
+
+
+
+    
+</style>
+

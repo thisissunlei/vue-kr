a0104ed3ff5d4423ebb95ab586982d7f22459c7f
yihaoliu <liuyihao@krspace.cn>
2018-07-02 20:24:31 +0800

    feat: 退组不输入负数

--- client/pages/bill/settlement-list/_billId/edit.vue
+++ client/pages/bill/settlement-list/_billId/edit.vue
@@ -246,7 +246,12 @@ export default {
 					if(isNaN(item.payableAmount)){
 						_this.error = true;
 						_this.errorMessage = "金额请填写数字"
-					}if(!item.feeType || !item.payableAmount){
+					}
+					// if(number(item.payableAmount)<0){
+					// 	_this.error = true;
+					// 	_this.errorMessage = "金额不得填写负数"
+					// }
+					if(!item.feeType || !item.payableAmount){
 						_this.error = true;
 						_this.errorMessage = "结算表单未填写完整"
 					}	

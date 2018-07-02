47f395a91062b44d82f03a819d228002b58cb591
yihaoliu <liuyihao@krspace.cn>
2018-07-02 16:11:28 +0800

    feat: 当面]

--- client/components/SelectSaleChance.vue
+++ client/components/SelectSaleChance.vue
@@ -59,6 +59,7 @@ export default {
             type:Boolean,
             default:true
         }
+        
     },
     data() {
         return {
@@ -156,7 +157,7 @@ export default {
                     isNewUser: r.data.items.isNewUser,
                     list: list
                 }
-                if(list.length ==2&&this.type != 'edit'){
+                if(list.length ==2&&this.type != 'edit' && r.data.items.isNewUser){
                     this.showValue = ''+list[1].value;
                 }
                  

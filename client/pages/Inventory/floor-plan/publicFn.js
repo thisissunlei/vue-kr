165c503a2cb1d778213204df76ea1aa02fa2b72f
wusuling <wusuling@krspace.cn>
2018-07-03 17:16:46 +0800

    feat: 平面图客户名称和固定办公桌

--- client/pages/Inventory/floor-plan/publicFn.js
+++ client/pages/Inventory/floor-plan/publicFn.js
@@ -1,30 +1,30 @@
 import dateUtils from 'vue-dateutils';
 //鼠标滑过气泡的位置
 function poptipOver(every,all,canvas,scroll,discount) {
-    var mainDom=document.getElementById('layout-content-main');
-    var tirDom = document.getElementById('gantt-chart-tool-tip'+every.item.id);
-    var contentDom=document.getElementById('gantt-chart-tool-tip-content'+every.item.id);
-    var angleDom = document.getElementById('gantt-chart-tool-tip-triangle'+every.item.id);
-    var canvasDom= document.querySelectorAll('#'+canvas+' canvas')[0];
-    var tirDetail=tirDom.getBoundingClientRect();
-    var canvasDetail=canvasDom.getBoundingClientRect();
+    let mainDom=document.getElementById('layout-content-main');
+    let tirDom = document.getElementById('gantt-chart-tool-tip'+every.item.id);
+    let contentDom=document.getElementById('gantt-chart-tool-tip-content'+every.item.id);
+    let angleDom = document.getElementById('gantt-chart-tool-tip-triangle'+every.item.id);
+    let canvasDom= document.querySelectorAll('#'+canvas+' canvas')[0];
+    let tirDetail=tirDom.getBoundingClientRect();
+    let canvasDetail=canvasDom.getBoundingClientRect();
     
     
-    var obj = getToolTipContent(every,discount); 
+    let obj = getToolTipContent(every,discount); 
     
     contentDom.innerHTML = obj.str;
     tirDetail=tirDom.getBoundingClientRect();
     contentDom=document.getElementById('gantt-chart-tool-tip-content'+every.item.id);
     
-    var toolLocation={
+    let toolLocation={
         left:Number(every.cellCoordX)+5-(Number(tirDetail.width)-Number(every.cellWidth))/2-scroll.left,
         top:Number(every.cellCoordY)+Number(canvasDetail.top)-Number(tirDetail.height)-15-70+mainDom.scrollTop //scroll.top
     }
    
     //临近边界偏移
-    var bodyWidth=document.body.clientWidth;
-    var boundTip=toolLocation.left+canvasDetail.left;
-    var offset=0;
+    let bodyWidth=document.body.clientWidth;
+    let boundTip=toolLocation.left+canvasDetail.left;
+    let offset=0;
     if(Number(bodyWidth)-Number(boundTip)<230){
         toolLocation.left=canvasDetail.width-tirDetail.width;
         offset=tirDetail.width/2-10;
@@ -45,39 +45,56 @@ function poptipOver(every,all,canvas,scroll,discount) {
 
 //气泡的具体内容
 function getToolTipContent(thatData,discount) {
-    var data = Object.assign({}, thatData);
-    var width = 264;
+    let data = Object.assign({}, thatData);
+    let width = 264;
     
-    var unitPrice=(data.item.unitPrice||data.item.unitPrice===0)?data.item.unitPrice:'';
-    var price=(data.item.price||data.item.price===0)?data.item.price:'';
-    var total=(data.item.totalPrice||data.item.totalPrice===0)?data.item.totalPrice:'';
-    var disUnitPrice=discount&&unitPrice?'折后'+parseInt(unitPrice*discount*0.1):unitPrice;
-    var disTotal=(discount&&total)?'折后'+parseInt(total*discount*0.1):total;
-    var rentStart = data.item.recentStart ? dateUtils.dateToStr('YYYY-MM-DD', new Date(data.item.recentStart)) : '';
-    var rentEnd = data.item.recentEnd ? dateUtils.dateToStr('YYYY-MM-DD', new Date(data.item.recentEnd)) : '';
-    var property=data.item.property?data.item.property:'';
-    var status=data.item.inventStatus;
+    let unitPrice=(data.item.unitPrice||data.item.unitPrice===0)?data.item.unitPrice:'';
+    let price=(data.item.price||data.item.price===0)?data.item.price:'';
+    let total=(data.item.totalPrice||data.item.totalPrice===0)?data.item.totalPrice:'';
+    let disUnitPrice=discount&&unitPrice?'折后'+parseInt(unitPrice*discount*0.1):unitPrice;
+    let disTotal=(discount&&total)?'折后'+parseInt(total*discount*0.1):total;
+    let rentStart = data.item.recentStart ? dateUtils.dateToStr('YYYY-MM-DD', new Date(data.item.recentStart)) : '';
+    let rentEnd = data.item.recentEnd ? dateUtils.dateToStr('YYYY-MM-DD', new Date(data.item.recentEnd)) : '';
+    let startDate = data.item.startDate ? dateUtils.dateToStr('YYYY-MM-DD', new Date(data.item.startDate)) : '';
+    let endDate = data.item.endDate ? dateUtils.dateToStr('YYYY-MM-DD', new Date(data.item.endDate)) : '';
+    let property=data.item.property?data.item.property:'';
+    let status=data.item.inventStatus;
     
-    var nameStr='';
-    var proStr='';
-    var signStr=(status=='IN_RENT'||status=='NOT_EFFECT'&&price)?'<div>签约价：' + price + '</div>':'';
-    var priceStr='';
-    var dateEnd=rentEnd?'<div>可租结束日：' + rentEnd + '</div>':'';
+    let nameStr='';
+    let proStr='';
+    let signStr='';
+    let dateRange='';
+    let priceStr='';
+    let customerName='';
+    let isName=data.item.customerName;
+    let dateEnd=rentEnd?'<div>可租结束日：' + rentEnd + '</div>':'';
+    let line='';
+    if(status=='IN_RENT'||status=='NOT_EFFECT'){
+        signStr=price?'<div>签约价：' + price + '</div>':'';
+        dateRange='<div>'+startDate+'至'+endDate+'</div>';
+        customerName=isName?'<div>'+isName+'</div>':'<div>*****</div>';
+        line='<span style="width:100%;height: 1px;display: inline-block;background: #fff;vertical-align: middle;"></span>';
+    }
+    
     if(data.item.belongType=='SPACE'){
         proStr='<div>商品属性：' + property + '</div>';
         priceStr='<div>工位单价：' + disUnitPrice + '</div>';
     }else{
-        nameStr='<div>固定办公桌:'+data.item.cellName+'</div>';
+        let typeName=data.item.goodsType=='OPEN'?'固定办公桌':'移动办公桌'
+        nameStr='<div>'+typeName+':'+data.item.cellName+'</div>';
     }
     
-    var str = '<div class="content">' + 
+    let str = '<div class="content">' + 
            nameStr+
          '<div>可租起始日：' +rentStart + '</div>' +
            dateEnd + 
            priceStr+
          '<div>商品定价：' + disTotal + '</div>'+
+           proStr+
+           line+
+           customerName+
+           dateRange+
            signStr+
-           proStr+  
          '</div>'
 
     return {
@@ -88,7 +105,7 @@ function getToolTipContent(thatData,discount) {
 
 //获取今天的日期
 function getToDay() {
-    var today = dateUtils.dateToStr("YYYY-MM-DD", new Date());
+    let today = dateUtils.dateToStr("YYYY-MM-DD", new Date());
     return today; 
 }
 

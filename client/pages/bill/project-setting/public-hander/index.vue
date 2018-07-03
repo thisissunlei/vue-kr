59d3f01430fb071e9095a8b315cbf1463c0ec93e
lemon <lemon123@qq.com>
2018-07-03 15:05:40 +0800

    fetch 项目评论样式修改

--- client/pages/bill/project-setting/public-hander/index.vue
+++ client/pages/bill/project-setting/public-hander/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="public-title">
         <div class='title-right'>
-            <Button  type="default" style="color:#4F9EED;border:1px solid #4F9EED;background-color:#fff"  @click="switchDelete">终止该项目</Button>
-            <Button  type="default" style="color:#4F9EED;border:1px solid #4F9EED;background-color:#fff" @click="openProject" >项目成员</Button>
-            <Button  type="default" style="color:#4F9EED;border:1px solid #4F9EED;background-color:#fff" @click="watchTask">查看编辑纪录</Button>
-            <Button type="primary" @click="goProjectDetail" v-if="isComment">编辑档案</Button>
+            <Button  type="default" v-if="isDeletePermission" style="color:#4F9EED;border:1px solid #4F9EED;background-color:#fff;margin-left:10px"  @click="switchDelete">终止该项目</Button>
+            <Button  type="default" style="color:#4F9EED;border:1px solid #4F9EED;background-color:#fff;margin-left:10px" @click="openProject" >项目成员</Button>
+            <Button  type="default" style="color:#4F9EED;border:1px solid #4F9EED;background-color:#fff;margin-left:10px" @click="watchTask">查看编辑纪录</Button>
+            <Button type="primary" style="margin-left:10px;" @click="goProjectDetail" v-if="isComment">编辑档案</Button>
         </div>
 
         <div class='title-left'>

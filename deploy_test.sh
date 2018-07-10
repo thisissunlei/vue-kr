e654e53e1aa410f97cfcf0db525ebd87c62fb41a
yihaoliu <liuyihao@krspace.cn>
2018-07-10 21:53:02 +0800

    feat: 合并master

--- deploy_test.sh
+++ deploy_test.sh
@@ -12,6 +12,7 @@ test01_ip=ali-docker-test01
 test02_ip=ali-docker-test02
 test03_ip=ali-docker-test03
 test04_ip=ali-docker-test04
+dev01_ip=10.4.15.241
 
 case $1 in
 
@@ -35,6 +36,10 @@ case $1 in
   ;;
    test04)
     target_site="www@${test04_ip}"
+    target_site_port=22
+  ;;
+    dev01)
+    target_site="www@${dev01_ip}"
     target_site_port=22
   ;;
   *)

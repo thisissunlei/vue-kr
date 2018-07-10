84436aae20cfda8ed1786b9b1c8878a17f57dd09
yihaoliu <liuyihao@krspace.cn>
2018-07-10 22:32:19 +0800

    feat: 合并master

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,16 +1,16 @@
 
 <template>
-    <div class="layout"  :class="{'layout-hide-text': spanLeft < 5}">
-        <div id="_layout_box_hander" ></div>
-        <Row type="flex">
-            <Col :span="spanRight">
-                <div id="layout-content_id" class="layout-content">
-                    <div id="layout-content-main" class="layout-content-main" :style="bodyStyle">
-                        <nuxt/>   
+    <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
+       
+            <Row type="flex">
+                <Col :span="spanRight">
+                    <div class="layout-content">
+                        <div id="layout-content-main" class="layout-content-main" :style="bodyStyle">
+                            <nuxt/>
+                        </div>
                     </div>
-                </div>
-            </Col>
-        </Row>
+                </Col>
+            </Row>
         <div class="layout-copy">
              &copy;  2011~2016 36氪 | 京ICP备12031756号 | 京公网安备11010802012285号
         </div>
@@ -28,15 +28,12 @@
                 bodyStyle:{
                     height:"200px",
                     overflow:'auto'
-                },
-                layoutId:'_layout_box'+Math.round(Math.random()*10),
+                }
 
             }
         },
         mounted(){
-            //  console.log(document.getElementById('_layout_box_hander'),this.layoutId)
-            // vueNavRender(document.getElementById('_layout_box_hander'),document.getElementById('layout-content_id'))
-            
+           
             this.bodyStyle.height = document.documentElement.clientHeight-130 + "px";
             var that = this;
             window.onresize=function(){
@@ -47,22 +44,19 @@
             
             }
         
-        }
+        },
         
     }
 </script>
 
 <style lang="less">
   body{
-     
+        padding-left:180px;
         min-height:100%;
         
         margin: 0px;
         #__nuxt{
             height:100%;
-        }
-        .layout-content{
-            
         }
     }
 </style>

97f59bc83801d9fff1e29190e8e71c19694f051d
wusuling <wusuling@krspace.cn>
2018-07-11 16:29:49 +0800

    feat: con

---
+++ client/pages/Inventory/floor-plan/configuration.vue
@@ -0,0 +1,26 @@
+<template>
+       <div>
+            123
+        </div>
+</template>
+
+<script>
+var count='';
+export default {
+    data() {
+        return{
+          
+        }
+    },
+    mounted(){
+        
+    },
+    methods:{
+        
+    }
+}
+</script>
+
+<style lang='less' scoped>
+    
+</style>

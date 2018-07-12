b46176d02dc4b53c2b321052624ad9425f7c7b9f
lemon <lemon123@qq.com>
2018-07-12 16:53:17 +0800

    fetch project-setting

--- client/pages/bill/project-setting/index.vue
+++ client/pages/bill/project-setting/index.vue
@@ -16,7 +16,10 @@
             </TabPane>
         </Tabs>
 
-      <Buttons label='导出'  type='primary' @click='submitExport' checkAction='pm_file_export'/>
+        <div style="padding:0 20px">
+          <Buttons label='导出'  type='primary' @click='submitExport' checkAction='pm_file_export'/>
+        </div>
+
 
 
 

f07171b927dfb98c111c716ed39c1dc731b01412
yihaoliu <liuyihao@krspace.cn>
2018-07-17 19:09:19 +0800

    feat: 错误不提交

--- client/pages/order-center/order-manage/station-order-manage/nullify.vue
+++ client/pages/order-center/order-manage/station-order-manage/nullify.vue
@@ -1,122 +1,95 @@
 <template>
-  <div class='nullifyform'>
-    <Form ref="formItem" :model="formItem" :rules="rules" :label-width="80">
-      <!-- <FormItem label="如真的需要作废请选择作废原因：">
-        <Select v-model="formItem.select">
-          <Option :value="item.value" v-for="(item,index ) in optionList" :key="index">{{item.desc}}</Option>
-        </Select>
-      </FormItem>
+	<div class='nullifyform'>
+		<Form ref="formItem" :model="formItem" :rules="rules" :label-width="80">
+			<p style="color:red">订单生效前，除客户基本信息以外的信息都可进行编辑</p>
+			<p class='required-label'>如真的需要作废请选择作废原因：</p>
+			<Select v-model="formItem.select">
+				<Option :value="item.value" v-for="(item,index ) in optionList" :key="index">{{item.desc}}</Option>
+			</Select>
+			<p>备注：</p>
+			<Input class='input' v-model="formItem.input" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="输入作废理由" />
 
-      <FormItem label="备注：">
-        <Input v-model="formItem.input" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="输入作废理由"></Input>
-      </FormItem>
-      <FormItem>
-        <Button type="primary" @click="handleSubmit('formItem')">确认</Button>
-        <Button type="ghost" style="margin-left: 8px">取消</Button>
-      </FormItem> -->
-      <p style="color:red">订单生效前，除客户基本信息以外的信息都可进行编辑</p>
-      <p class='required-label'>如真的需要作废请选择作废原因：</p>
-      <Select v-model="formItem.select">
-        <Option :value="item.value" v-for="(item,index ) in optionList" :key="index">{{item.desc}}</Option>
-      </Select>
-      <p>备注：</p>
-      <Input class='input' v-model="formItem.input" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="输入作废理由"></Input>
+			<div class='buttonContainer'>
 
-      <div class='buttonContainer'>
+				<Button type="primary" @click="handleSubmit('formItem')">确认</Button>
+				<Button type="ghost" style="margin-left: 8px" @click="handleCancel">取消</Button>
+			</div>
 
-        <Button type="primary" @click="handleSubmit('formItem')">确认</Button>
-        <Button type="ghost" style="margin-left: 8px" @click="handleCancel">取消</Button>
-      </div>
-
-      <FormItem>
-
-      </FormItem>
-
-    </Form>
-
-  </div>
+		</Form>
+	</div>
 </template>
-
-
 
 <script>
 export default {
-  props: ['id'],
-  data() {
-    return {
-      formItem: {
-        input: "",
-        select: ""
-      },
-      rules: {
-        select: [
-          { required: true, message: '请选择作废类型', trigger: 'blur' }
-        ],
-        input: [
-          { required: true, message: '请输入作废理由', trigger: 'blur' },
-          { type: 'string', max: 250, message: '长度不能超过250！', trigger: 'blur' }
-        ]
-      },
-      optionList: [
-        { value: "beijing", label: "北京" },
-        { value: "beijing1", label: "北京1" },
-        { value: "beijing2", label: "北京2" }
-      ]
-    };
-  },
-  mounted() {
-    this.getOptionList()
-  },
-  methods: {
-    getOptionList() {
-      let params = {
-        enmuKey: 'com.krspace.op.api.enums.seatorder.InvalidType',
-        // enmuKey: 'com.krspace.erp.api.enums.pm.PmDepartment'
-      };
-      this.$http
-        .post("get-enum-all-data", params)
-        .then(response => {
-          this.optionList = response.data;
-        })
-        .catch(error => {
-          this.$Notice.error({
-            title: error.message
-          });
-        });
-    },
-    handleCancel() {
-      this.$emit('closeModalForm');
-    },
-    handleSubmit(name) {
-      {
-        let params = {
-          id: this.id,
-          invalidType: this.formItem.select,
-          remark: this.formItem.input
-        };
-        console.log(params)
-        this.$http
-          .post("join-nullify", params)
-          .then(response => {
-            console.log(response.data)
-            this.formItem.select = '';
-            this.formItem.input = '';
-            this.$emit('refershList', { id: params.id });
-          })
-          .catch(error => {
-            // this.openMessage = true;
-            // this.MessageType = "error";
-            // this.warn = error.message;
-            this.$Notice.error({
-              title: error.message
-            });
-            // this.formItem.select = '';
-            // this.formItem.input = '';
-          });
-      }
-
-    }
-  }
+	
+	data() {
+		return {
+			formItem: {
+				input: "",
+				select: ""
+			},
+			rules: {
+				select: [
+					{ required: true, message: '请选择作废类型', trigger: 'blur' }
+				],
+				input: [
+					{ required: true, message: '请输入作废理由', trigger: 'blur' },
+					{ type: 'string', max: 250, message: '长度不能超过250！', trigger: 'blur' }
+				]
+			},
+			optionList: [
+				{ value: "beijing", label: "北京" },
+				{ value: "beijing1", label: "北京1" },
+				{ value: "beijing2", label: "北京2" }
+			]
+		};
+	},
+	mounted() {
+		this.getOptionList()
+	},
+	methods: {
+		getOptionList() {
+			let params = {
+					enmuKey: 'com.krspace.op.api.enums.seatorder.InvalidType',
+					// enmuKey: 'com.krspace.erp.api.enums.pm.PmDepartment'
+				};
+			this.$http.post("get-enum-all-data", params).then(response => {
+			this.optionList = response.data;
+			}).catch(error => {
+				this.$Notice.error({
+					title: error.message
+				});
+			});
+			
+		},
+		handleCancel() {
+			this.$emit('closeModalForm');
+		},
+		handleSubmit(name) {
+		
+			let params = {
+				id: this.id,
+				invalidType: this.formItem.select,
+				remark: this.formItem.input
+			};
+			this.$http.post("join-nullify", params).then(response => {
+			
+				this.formItem.select = '';
+				this.formItem.input = '';
+				this.$emit('refershList', { id: params.id });
+			}).catch(error => {
+				// this.openMessage = true;
+				// this.MessageType = "error";
+				// this.warn = error.message;
+					this.$Notice.error({
+						title: error.message
+					});
+				// this.formItem.select = '';
+				// this.formItem.input = '';
+			});
+		}
+	}
+   
 }
 </script>
 

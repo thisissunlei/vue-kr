18f7f30d3334e1c7236272eadad176c8507ddeec
谭琳琳 <805148199@qq.com>
2018-07-19 17:13:12 +0800

    编辑修改

--- client/pages/order-center/order-manage/station-order-manage/_orderEdit/join.vue
+++ client/pages/order-center/order-manage/station-order-manage/_orderEdit/join.vue
@@ -246,6 +246,7 @@ import selectCustomers from '~/components/SelectCustomers.vue'
 
 import SelectSaler from '~/components/SelectSaler.vue'
 import DetailStyle from '~/components/DetailStyle';
+import SelectChance from '~/components/SelectSaleChance.vue';
 
 import planMap from '~/components/PlanMap.vue';
 import dateUtils from 'vue-dateutils';

71ebb05b8fe254bdd5f1c9d6493603b0f95ce2bc
yihaoliu <liuyihao@krspace.cn>
2018-07-23 16:58:34 +0800

    feat: ico

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 // var path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
 module.exports = {
   srcDir: 'client/',
   cache:{ max: 20, maxAge: 700000 },
@@ -20,7 +19,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'http://img.krspace.cn/common/favicon_new.ico' }
+      // { rel: 'icon', type: 'image/x-icon', href: 'https://img.krspace.cn/common/favicon_new.ico' }
     ]
   },
 

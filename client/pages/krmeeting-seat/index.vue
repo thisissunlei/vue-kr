f6917f6f8cb93b75abe5e44783ff0395e2f428cd
machaoyue <machaoyue@36kr.com>
2018-07-23 12:15:32 +0800

    fix: 新建文件夹

---
+++ client/pages/krmeeting-seat/index.vue
@@ -0,0 +1,30 @@
+<template>
+    
+</template>
+
+<script>
+import SectionTitle from '~/components/SectionTitle';
+import dateUtils from 'vue-dateutils';
+
+
+export default {
+    head () {
+            return {
+                title: "KM会议室订单"
+            }
+    },
+   data(){
+       return {
+        }
+   },
+   mounted(){
+   },
+   methods:{
+        
+        
+    }
+ }
+</script>
+<style>
+text-align: right
+</style>

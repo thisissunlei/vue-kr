7c250ecac59eac59fe5979f24a636f75d3fd3877
yihaoliu <liuyihao@krspace.cn>
2018-08-06 12:13:27 +0800

    feat: um

--- config/plugins.env.js
+++ config/plugins.env.js
@@ -26,7 +26,7 @@ const envs = {
 }
 const pluginsDomain  = envs[process.env.NODE_ENV].plugins+'/plugins';
 const jsFiles = [
-//    {src: pluginsDomain+'/nav/1.0.0/nav.js'},
+   {src: pluginsDomain+'/nav/1.0.0/nav.js'},
    {src: 'https://web.krspace.cn/kr-op/echarts/4.1.0/echarts.min.js'},
    {src: 'https://web.krspace.cn/kr-op/go/1.8.14/go.js'},
 ]

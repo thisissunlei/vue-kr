08479ed4f60f211eb54cbaa465a3b7131eba6845
wusuling <wusuling@krspace.cn>
2018-08-14 19:15:51 +0800

    feat: 管理员title

--- client/pages/order-center/order-manage/station-order-manage/_orderEdit/renew.vue
+++ client/pages/order-center/order-manage/station-order-manage/_orderEdit/renew.vue
@@ -225,7 +225,7 @@
         <!--苏岭增加客户主管理员开始-->
         <Modal
             v-model="isAddManager"
-            title="主管理员变更"
+            :title="manageTitle"
             width="665"
         >
             <AddManager  
@@ -282,6 +282,7 @@ export default {
             isAddEdit:false,
             managerId:'',
             oldManagerId:'',
+            manageTitle:"",
             //苏岭结束
 
             orderId:'',
@@ -523,6 +524,7 @@ export default {
 			})
         },
         addEditOpen(){
+           this.manageTitle=this.isAddEdit?'主管理员变更':'主管理员添加';
            this.isAddManager=!this.isAddManager;
         },
         getformData(form){

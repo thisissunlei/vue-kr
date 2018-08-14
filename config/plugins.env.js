e7f9914795c2a67e67bb22c0448d369b46aeb850
yihaoliu <liuyihao@krspace.cn>
2018-08-14 14:07:58 +0800

    chore: nav引用

--- config/plugins.env.js
+++ config/plugins.env.js
@@ -27,12 +27,10 @@ const envs = {
 const pluginsDomain  = envs[process.env.NODE_ENV].plugins+'/plugins';
 const jsFiles = [
     {src: pluginsDomain+'/nav/1.0.0/nav.js'},
-    // {src:'/nav/nav.js'},
     {src: 'https://web.krspace.cn/kr-op/echarts/4.1.0/echarts.min.js'},
     {src: 'https://web.krspace.cn/kr-op/go/1.8.14/go.js'},
 ]
 const cssFiles = [
-    // {rel:'stylesheet', herf: 'https://web.krspace.cn/kr-op/iview/2.8.0/styles/iview.css'},
     {rel:'stylesheet',href:pluginsDomain+'/nav/1.0.0/nav.css'},
     { rel: 'stylesheet', href: pluginsDomain+'/public/css/main.css' },
     { rel: 'icon', type: 'image/x-icon', href: '/plugins/public/images/favicon.ico' },

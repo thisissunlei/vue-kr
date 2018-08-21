4e332c2730e9b27ab1cc714d7b91f842e76da33c
dongfanai <dongfanai@krspace.cn>
2018-08-21 18:43:27 +0800

    feat: 优惠券

---
+++ client/pages/krmeeting-sale/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="g-krmeeting-sale">
+        <SectionTitle title="优惠券"></SectionTitle>
+    </div>
+</template>
+<script>
+ import SectionTitle from '~/components/SectionTitle';
+export default {
+     head () {
+          return {
+              title: "优惠券-氪空间后台管理系统"
+          }
+      },
+      components:{
+        SectionTitle,
+       
+      },
+      data(){
+          return{
+
+          }
+      },
+      mounted(){
+
+      },
+      methods:{
+
+      }
+}
+</script>
+<style lang="less">
+
+</style>
+
+

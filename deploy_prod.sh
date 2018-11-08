46eb72411e60e8c550f6be37b9c58b7a366ccab6
yihaoliu <liuyihao@krspace.cn>
2018-11-08 23:08:34 +0800

    feat: sim

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -8,7 +8,10 @@ if [ $1 = 'prod01' ]; then
 rsync -rvltOD ./ www@ali-krspace-web-01:/data/work/frontend/${build_dir}/
 elif [ $1 = 'prod02' ]; then
 rsync -rvltOD ./ www@ali-krspace-web-02:/data/work/frontend/${build_dir}/
+elif [ $1 = 'sim' ]; then
+rsync -rvltOD ./ www@ali-krspace-sim-01:/data/work/frontend/${build_dir}/
 elif [ $1 = 'all' ]; then
+rsync -rvltOD ./ www@ali-krspace-sim-01:/data/work/frontend/${build_dir}/
 rsync -rvltOD ./ www@ali-krspace-web-01:/data/work/frontend/${build_dir}/
 rsync -rvltOD ./ www@ali-krspace-web-02:/data/work/frontend/${build_dir}/
 fi

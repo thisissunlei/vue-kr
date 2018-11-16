2acb4ecb89299a2c0f59e86a152ce52d377ab3ca
sunlei <sunlei@MacBook-Pro-10.local>
2018-11-16 16:35:14 +0800

    ajax

--- client/plugins/interceptors/index.js
+++ client/plugins/interceptors/index.js
@@ -4,10 +4,8 @@ import axios from "axios";
 //添加一个请求拦截器，用于设置请求过渡状态
 const requestInterceptor = axios.interceptors.request.use(config => {
   if (config.method == 'post'||config.method == 'put') {
-    if( (typeof config.data)==='object') {
-      let data = Qs.stringify(config.data);
-      config.data = data;
-    } 
+    let data = Qs.stringify(config.data);
+    config.data = data;
   }
   return config
 }, error => {

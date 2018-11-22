f202f7b47debcfe3d6eb7dbea0be52a4d5ccf642
sunlei <sunlei@kr.space.com>
2018-11-22 11:16:48 +0800

    feat: status

--- client/plugins/interceptors/status.js
+++ client/plugins/interceptors/status.js
@@ -6,26 +6,26 @@ class statusFn{
     getStatus(response){
       switch (response.status) {
          case 401:
-             const redirectUrl = encodeURIComponent(window.location.href);
-             window.location.href = `/new/login.html?RU=${redirectUrl}`;
-             return ;
+            if(response.data.code==='not login'){
+                const redirectUrl = encodeURIComponent(window.location.href);
+                window.location.href = `/new/login.html?RU=${redirectUrl}`;
+                return ;
+            }
+            break;
          case 404:
-             response.data.message="发出的请求针对的是不存在的记录"
-             break;
-         case 400:
-             response.data.message="发出的请求有错误，服务器没有进行新建或修改数据的操作"
+             response.data.msg="发出的请求针对的是不存在的记录"
              break;
          case 500:
-             response.data.message="服务器发生错误，请检查服务器"
+             response.data.msg="服务器发生错误，请检查服务器"
              break;
          case 502:
-             response.data.message="'网关错误"
+             response.data.msg="'网关错误"
              break;
          case 504:
-             response.data.message="网关超时"
+             response.data.msg="网关超时"
              break;
          case 503:
-             response.data.message="'服务不可用，服务器暂时过载或维护"
+             response.data.msg="'服务不可用，服务器暂时过载或维护"
              break;
          default:
              break;
